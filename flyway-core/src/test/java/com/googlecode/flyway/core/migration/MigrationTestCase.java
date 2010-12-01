fa5fc4a4d2aa6d9aeebe40e565478fdca7849be5
Axel Fontaine <business.axelfontaine.eu@gmail.com>
2010-12-01 01:07:43 +0100

    Fixed testcase for postgresql

--- flyway-core/src/test/java/com/googlecode/flyway/core/migration/MigrationTestCase.java
+++ flyway-core/src/test/java/com/googlecode/flyway/core/migration/MigrationTestCase.java
@@ -34,7 +34,11 @@ import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.util.HashMap;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Test to demonstrate the migration functionality.
@@ -235,15 +239,11 @@ public abstract class MigrationTestCase {
         }
 
         flyway.setIgnoreFailedFutureMigration(true);
-        if (getDbSupport(new JdbcTemplate(migrationDataSource)).supportsDdlTransactions()) {
+        try {
             flyway.migrate();
-        } else {
-            try {
-                flyway.migrate();
-                fail();
-            } catch (IllegalStateException e) {
-                //Expected
-            }
+            fail();
+        } catch (IllegalStateException e) {
+            //Expected
         }
     }
 
