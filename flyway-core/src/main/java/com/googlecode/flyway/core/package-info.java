52c2169ce129452f7ac1e94e4adb98784472c983
Axel Fontaine <business.axelfontaine.eu@gmail.com>
2010-12-26 21:02:27 +0100

    Added missing copyright headers

--- flyway-core/src/main/java/com/googlecode/flyway/core/package-info.java
+++ flyway-core/src/main/java/com/googlecode/flyway/core/package-info.java
@@ -1,4 +1,20 @@
 /**
+ * Copyright (C) 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * The main Flyway package and for most users, the only one they'll ever need to know about.
  */
 package com.googlecode.flyway.core;
