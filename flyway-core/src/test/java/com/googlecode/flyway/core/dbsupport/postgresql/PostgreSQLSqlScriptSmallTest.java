877c78aef5a5aff935a9fdb33982bd02b41ca2c9
Axel Fontaine <axel.fontaine.business@gmail.com>
2012-04-05 10:55:14 -0300

    Added test for multiline string literal with semicolons parsing for postgresql

--- flyway-core/src/test/java/com/googlecode/flyway/core/dbsupport/postgresql/PostgreSQLSqlScriptSmallTest.java
+++ flyway-core/src/test/java/com/googlecode/flyway/core/dbsupport/postgresql/PostgreSQLSqlScriptSmallTest.java
@@ -18,6 +18,7 @@ package com.googlecode.flyway.core.dbsupport.postgresql;
 import com.googlecode.flyway.core.migration.sql.PlaceholderReplacer;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -31,5 +32,15 @@ public class PostgreSQLSqlScriptSmallTest {
         assertTrue(script.endsWithOpenMultilineStringLiteral("INSERT INTO address VALUES (1, '1. first"));
         assertFalse(script.endsWithOpenMultilineStringLiteral("INSERT INTO address VALUES (1, '1. first\n" +
                 "2. second');"));
+    }
+
+    @Test
+    public void multilineStringLiteralWithSemicolons() {
+        String sqlScriptSource = "INSERT INTO address VALUES (1, '1. first;\n"
+                + "2. second;\n"
+                + "3. third;')";
+        final PostgreSQLSqlScript script = new PostgreSQLSqlScript(sqlScriptSource, PlaceholderReplacer.NO_PLACEHOLDERS);
+        assertEquals(1, script.getSqlStatements().size());
+        assertEquals(sqlScriptSource, script.getSqlStatements().get(0).getSql());
     }
 }
