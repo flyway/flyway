7100dce9d8237ff135a31e85461f2286fb5cbf13
Axel Fontaine <axel.fontaine.business@gmail.com>
2012-09-18 12:08:59 +0200

    Fixed imports

--- flyway-sample-webapp/src/main/java/com/googlecode/flyway/sample/webapp/EnvInfoServlet.java
+++ flyway-sample-webapp/src/main/java/com/googlecode/flyway/sample/webapp/EnvInfoServlet.java
@@ -23,7 +23,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 
