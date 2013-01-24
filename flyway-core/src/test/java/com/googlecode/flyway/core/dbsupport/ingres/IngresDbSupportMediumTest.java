2b697a14308257f3ef77ecd35dc5d115368dce90
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2013-01-24 11:10:54 +0200

    Revert "Removed faulty tests for Ingres."
    This reverts commit 8c41b8f66b5f2cf5ec1e2292732895df463fcd2e.

---
+++ flyway-core/src/test/java/com/googlecode/flyway/core/dbsupport/ingres/IngresDbSupportMediumTest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C) 2010-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.flyway.core.dbsupport.ingres;
+
+import com.googlecode.flyway.core.dbsupport.Schema;
+import com.googlecode.flyway.core.util.jdbc.DriverDataSource;
+import com.googlecode.flyway.core.util.jdbc.JdbcUtils;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test for IngresDbSupport.
+ */
+public class IngresDbSupportMediumTest {
+    /**
+     * Checks that the search_path is extended and not overwritten so that objects in PUBLIC can still be found.
+     */
+    @Test
+    public void setCurrentSchema() throws Exception {
+        Connection connection = createDataSource().getConnection();
+        IngresDbSupport dbSupport = new IngresDbSupport(connection);
+        Schema schema = dbSupport.getSchema("search_path_test");
+        schema.create();
+        dbSupport.setCurrentSchema(dbSupport.getSchema("search_path_test"));
+        String searchPath = dbSupport.getJdbcTemplate().queryForString("SHOW search_path");
+        assertEquals("search_path_test, \"$user\", public", searchPath);
+        schema.drop();
+        JdbcUtils.closeConnection(connection);
+    }
+
+
+    /**
+     * Creates a datasource for use in tests.
+     *
+     * @return The new datasource.
+     */
+    private DataSource createDataSource() throws Exception {
+        File customPropertiesFile = new File(System.getProperty("user.home") + "/flyway-mediumtests.properties");
+        Properties customProperties = new Properties();
+        if (customPropertiesFile.canRead()) {
+            customProperties.load(new FileInputStream(customPropertiesFile));
+        }
+        String user = customProperties.getProperty("ingres.user", "flyway");
+        String password = customProperties.getProperty("ingres.password", "flyway");
+        String url = customProperties.getProperty("ingres.url", "jdbc:ingres://localhost:II7/flyway_db");
+
+        return new DriverDataSource(null, url, user, password);
+    }
+
+
+}
---
+++ flyway-core/src/test/java/com/googlecode/flyway/core/dbsupport/ingres/IngresSuperUserMigrationMediumTest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) 2010-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.flyway.core.dbsupport.ingres;
+
+import com.googlecode.flyway.core.Flyway;
+import com.googlecode.flyway.core.util.jdbc.DriverDataSource;
+import com.googlecode.flyway.core.validation.ValidationMode;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+/**
+ * Ingres medium tests that require SuperUser permissions.
+ */
+@SuppressWarnings({"JavaDoc"})
+public class IngresSuperUserMigrationMediumTest {
+    private Flyway flyway;
+
+    @Before
+    public void setUp() throws Exception {
+        File customPropertiesFile = new File(System.getProperty("user.home") + "/flyway-mediumtests.properties");
+        Properties customProperties = new Properties();
+        if (customPropertiesFile.canRead()) {
+            customProperties.load(new FileInputStream(customPropertiesFile));
+        }
+
+        String password = customProperties.getProperty("ingres.password", "flyway");
+        String url = customProperties.getProperty("ingres.url", "jdbc:ingres://localhost:II7/flyway_db");
+
+        flyway = new Flyway();
+        flyway.setSchemas("super_user_test");
+        flyway.setDataSource(new DriverDataSource(null, url, "ingres", password));
+        flyway.setValidationMode(ValidationMode.ALL);
+        flyway.clean();
+    }
+
+    /**
+     * Tests clean and migrate for Ingres Types.
+     */
+    @Test
+    public void type() throws Exception {
+        flyway.setLocations("migration/dbsupport/ingres/sql/type");
+        flyway.migrate();
+
+        flyway.clean();
+
+        // Running migrate again on an unclean database, triggers duplicate object exceptions.
+        flyway.migrate();
+
+        // Clean again, to prevent tests with non superuser rights to fail.
+        flyway.clean();
+    }
+}
