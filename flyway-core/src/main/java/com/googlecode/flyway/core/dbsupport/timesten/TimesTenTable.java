2f164cd9379823d8244859c86f0e3b379cc79475
Nayan Hajratwala <nayan@chikli.com>
2013-06-06 14:00:59 -0400

    removed cascade constraints from drop table

--- flyway-core/src/main/java/com/googlecode/flyway/core/dbsupport/timesten/TimesTenTable.java
+++ flyway-core/src/main/java/com/googlecode/flyway/core/dbsupport/timesten/TimesTenTable.java
@@ -40,7 +40,7 @@ public class TimesTenTable extends Table {
 
     @Override
     protected void doDrop() throws SQLException {
-        jdbcTemplate.execute("DROP TABLE " + dbSupport.quote(schema.getName(), name) + " CASCADE CONSTRAINTS PURGE");
+        jdbcTemplate.execute("DROP TABLE " + dbSupport.quote(schema.getName(), name));
     }
 
     @Override
