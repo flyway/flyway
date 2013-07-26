3fb96f5979eb874ac7595ef1ebae3a640469ce09
Axel Fontaine <business@axelfontaine.com>
2013-07-26 10:43:37 +0200

    Added license info to the project per GitHub standard LICENSE file

--- flyway-sbt-plugin/project/FlywayPluginBuild.scala
+++ flyway-sbt-plugin/project/FlywayPluginBuild.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-2013 the original author or authors.
+ * Copyright 2010-2013 Axel Fontaine and the many contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import sbt._
 import scala._
 import Keys._
