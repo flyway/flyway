32e95a800e64e61edab26e3649bc85cfade0c7d9
SKullal <SKullal@PVD-T35k-SK.red.usa>
2013-11-01 09:58:46 -0400

    Fix for Trigger Creation. NonStop does not use special Delimeters for Procedures,Triggers etc

--- flyway-core/src/test/java/com/googlecode/flyway/core/dbsupport/nonstop/NonstopMigrationMediumTest.java
+++ flyway-core/src/test/java/com/googlecode/flyway/core/dbsupport/nonstop/NonstopMigrationMediumTest.java
@@ -19,112 +19,38 @@
  */
 package com.googlecode.flyway.core.dbsupport.nonstop;
 
-import com.googlecode.flyway.core.migration.MigrationTestCase;
-import com.googlecode.flyway.core.migration.SchemaVersion;
-import com.googlecode.flyway.core.util.jdbc.DriverDataSource;
+import com.googlecode.flyway.core.Flyway;
 import org.junit.Test;
 
-import javax.sql.DataSource;
 import java.util.Properties;
 
-import static org.junit.Assert.assertEquals;
 
 /**
- * Test to demonstrate the migration functionality using DB2.
+ * Test to demonstrate the migration functionality using HP NonStop.
  */
-public class NonstopMigrationMediumTest extends MigrationTestCase {
-    @Override
-    protected DataSource createDataSource(Properties customProperties) throws Exception {
-        String user = customProperties.getProperty("nonstop.user", "dba.manager");
-        String password = customProperties.getProperty("nonstop.password", "Today006!");
-        String url = customProperties.getProperty("nonstop.url", "jdbc:t4sqlmx://10.221.221.161:18650/:serverDataSource=PRISMDS;catalog=flywayTestCatalog;schema=schema1");
+public class NonstopMigrationMediumTest {
+    
+    private Flyway getFlyway(){
+        Properties properties = new Properties();
+        properties.setProperty("flyway.user", "dba.manager");
+        properties.setProperty("flyway.password", "Today006!");
+        properties.setProperty("flyway.url", "jdbc:t4sqlmx://10.221.221.161:18650/:serverDataSource=PRISMDS;catalog=flywayTestCatalog;schema=schema1");
+        properties.setProperty("flyway.driver", "com.tandem.t4jdbc.SQLMXDriver");
 
-        return new DriverDataSource(null, url, user, password);
-    }
-
-    @Override
-    protected String getQuoteLocation() {
-        return "migration/quote";
-    }
-
-    @Test
-    public void sequence() throws Exception {
-        flyway.setLocations("migration/dbsupport/nonstop/sql/sequence");
-        flyway.migrate();
-
-        SchemaVersion schemaVersion = flyway.status().getVersion();
-        assertEquals("1", schemaVersion.toString());
-        assertEquals("Sequence", flyway.status().getDescription());
-
-        assertEquals(666, jdbcTemplate.queryForInt("VALUES NEXTVAL FOR BEAST_SEQ"));
-
-        flyway.clean();
-        flyway.migrate();
-    }
-
-    @Test
-    public void mqt() throws Exception {
-        flyway.setLocations("migration/dbsupport/nonstop/sql/mqt");
-        flyway.migrate();
-
-        SchemaVersion schemaVersion = flyway.status().getVersion();
-        assertEquals("1", schemaVersion.toString());
-        assertEquals("Mqt", flyway.status().getDescription());
-
-        assertEquals(2, jdbcTemplate.queryForInt("SELECT COUNT(*) FROM empl_mqt"));
-
-        flyway.clean();
-        flyway.migrate();
-    }
-
-    @Test
-    public void alias() throws Exception {
-        flyway.setLocations("migration/dbsupport/nonstop/sql/alias");
-        flyway.migrate();
-
-        SchemaVersion schemaVersion = flyway.status().getVersion();
-        assertEquals("1", schemaVersion.toString());
-        assertEquals("Alias", flyway.status().getDescription());
-
-        assertEquals(2, jdbcTemplate.queryForInt("SELECT COUNT(*) FROM POOR_SLAVE"));
-
-        flyway.clean();
-        flyway.migrate();
+        final Flyway flyway = new Flyway();
+        flyway.configure(properties);
+        return flyway;
     }
 
     @Test
     public void trigger() throws Exception {
+        Flyway flyway = getFlyway();
         flyway.setLocations("migration/dbsupport/nonstop/sql/trigger");
         flyway.migrate();
 
-        flyway.clean();
-        flyway.migrate();
+        //flyway.clean();
+        //flyway.migrate();
     }
 
-    @Test
-    public void procedure() throws Exception {
-        flyway.setLocations("migration/dbsupport/nonstop/sql/procedure");
-        flyway.migrate();
-
-        flyway.clean();
-        flyway.migrate();
-    }
-
-    @Test
-    public void type() throws Exception {
-        flyway.setLocations("migration/dbsupport/nonstop/sql/type");
-        flyway.migrate();
-
-        flyway.clean();
-        flyway.migrate();
-    }
-
-    @Test
-    public void function() throws Exception {
-        flyway.setLocations("migration/dbsupport/nonstop/sql/function");
-        flyway.migrate();
-
-        flyway.clean();
-        flyway.migrate();
-    }
+   
 }
