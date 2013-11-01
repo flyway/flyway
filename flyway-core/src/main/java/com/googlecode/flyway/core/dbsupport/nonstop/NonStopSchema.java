8dc44efc6ea1e9d34e9ccd2f394cd7dccdc4d2da
SKullal <SKullal@PVD-T35k-SK.red.usa>
2013-11-01 10:44:34 -0400

    Revert from String.isEmpty() Since Flyway uses 1.5 and isEmpty() was added in 1.6

--- flyway-core/src/main/java/com/googlecode/flyway/core/dbsupport/nonstop/NonStopSchema.java
+++ flyway-core/src/main/java/com/googlecode/flyway/core/dbsupport/nonstop/NonStopSchema.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2010-2013 Axel Fontaine and the many contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 /**
  *
@@ -112,7 +112,7 @@ public class NonStopSchema extends Schema {
         String schemaName, catalogName;
         if (rs != null) {
             while (rs.next()) {
-                if (!rootCatalog.isEmpty() && !rootSchema.isEmpty()) {
+                if (rootCatalog.length() > 0 && rootSchema.length() > 0) {
                     //Retreived Both Root Catalog and RootSchema. Break from Loop.
                     break;
                 }
