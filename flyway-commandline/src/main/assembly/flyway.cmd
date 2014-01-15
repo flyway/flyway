2210086a5b8a8e21c6a8ffa8a0c45832b903bc8e
Axel Fontaine <business@axelfontaine.com>
2014-01-15 14:23:12 +0100

    Updated copyright to 2014

--- flyway-commandline/src/main/assembly/flyway.cmd
+++ flyway-commandline/src/main/assembly/flyway.cmd
@@ -1,5 +1,5 @@
 @REM
-@REM Copyright 2010-2013 Axel Fontaine and the many contributors.
+@REM Copyright 2010-2014 Axel Fontaine and the many contributors.
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
 @REM you may not use this file except in compliance with the License.
