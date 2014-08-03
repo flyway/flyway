a9525b7da4d91677e1aeaf95097fed09bd6e2eda
Axel Fontaine <business@axelfontaine.com>
2014-08-03 22:32:21 +0200

    Fix #801 ClassPathScanner fails with enum

---
+++ flyway-core/src/test/java/org/flywaydb/core/internal/resolver/jdbc/dummy/SabotageEnum.java
@@ -0,0 +1,18 @@
+package org.flywaydb.core.internal.resolver.jdbc.dummy;
+
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+
+import java.sql.Connection;
+
+/**
+ * Trips up the ClassPathScanner. See issue 801.
+ */
+@SuppressWarnings("UnusedDeclaration")
+public enum SabotageEnum implements JdbcMigration {
+    FAIL {
+        @Override
+        public void migrate(Connection connection) throws Exception {
+
+        }
+    }
+}
