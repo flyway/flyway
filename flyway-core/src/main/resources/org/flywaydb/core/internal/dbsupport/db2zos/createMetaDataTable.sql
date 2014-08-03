bd293cf500036a01edc918018e257c4b16ccafbb
Christine Teig <christine.teig@gmail.com>
2014-08-03 22:55:35 +0200

    Set current sqlId to specify schema when create metadata table for db2 zOS

--- flyway-core/src/main/resources/org/flywaydb/core/internal/dbsupport/db2zos/createMetaDataTable.sql
+++ flyway-core/src/main/resources/org/flywaydb/core/internal/dbsupport/db2zos/createMetaDataTable.sql
@@ -13,6 +13,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
+set current sqlid = '${schema}';
 
 CREATE TABLESPACE SDBVERS
       IN "${schema}"
