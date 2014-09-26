ceebd7615e0363c05324206c0ba55d6effa308ae
Axel Fontaine <business@axelfontaine.com>
2014-09-27 00:44:42 +0200

    Fixed parsing of $_$abc $_$ string literals for PostgreSQL

--- flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/postgresql/PostgreSQLSqlStatementBuilder.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/postgresql/PostgreSQLSqlStatementBuilder.java
@@ -28,13 +28,13 @@ public class PostgreSQLSqlStatementBuilder extends SqlStatementBuilder {
      * Matches $$, $BODY$, $xyz123$, ...
      */
     /*private -> for testing*/
-    static final String DOLLAR_QUOTE_REGEX = "\\$[A-Za-z0-9_]*\\$.*";
+    static final String DOLLAR_QUOTE_REGEX = "(\\$[A-Za-z0-9_]*\\$).*";
 
     @Override
     protected String extractAlternateOpenQuote(String token) {
         Matcher matcher = Pattern.compile(DOLLAR_QUOTE_REGEX).matcher(token);
         if (matcher.find()) {
-            return token.substring(matcher.start(), matcher.end());
+            return matcher.group(1);
         }
         return null;
     }
