eb513b6c1542b837370dc00dfa12d9e4eacb7e8c
Axel Fontaine <business@axelfontaine.com>
2014-10-14 18:06:07 +0200

    Fixed #799 Clean Oracle scheduled jobs

--- flyway-core/src/main/resources/org/flywaydb/core/internal/dbsupport/db2zos/createMetaDataTable.sql
+++ flyway-core/src/main/resources/org/flywaydb/core/internal/dbsupport/db2zos/createMetaDataTable.sql
@@ -13,7 +13,6 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
-set current sqlid = '${schema}';
 
 CREATE TABLESPACE SDBVERS
       IN "${schema}"
