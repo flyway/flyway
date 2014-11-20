7caae848a91c05ae1ee0abd202007a69d18eb3bb
Forstner, Michael <forstnerm@media-saturn.com>
2014-11-20 16:03:06 +0100

    Initial commit for SolidDB support (#885)

---
+++ flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/solid/SolidDbSupport.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2010-2014 Axel Fontaine
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//
+// Project: spring-boot-sample-flyway
+//
+// Media-Saturn IT Services GmbH
+//
+// Wankelstr. 5
+// 85046 Ingolstadt
+// Telefon: +49 (841) 634-0
+// Telefax: +49 (841) 634-992596
+// Web:     www.media-saturn.com
+//
+
+package org.flywaydb.core.internal.dbsupport.solid;
+
+import org.flywaydb.core.internal.dbsupport.DbSupport;
+import org.flywaydb.core.internal.dbsupport.JdbcTemplate;
+import org.flywaydb.core.internal.dbsupport.Schema;
+import org.flywaydb.core.internal.dbsupport.SqlStatementBuilder;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class SolidDbSupport extends DbSupport {
+
+    public SolidDbSupport(final Connection connection) {
+        super(new JdbcTemplate(connection, Types.NULL));
+    }
+
+    @Override
+    public Schema getSchema(final String name) {
+        return new SolidSchema(jdbcTemplate, this, name);
+    }
+
+    @Override
+    public SqlStatementBuilder createSqlStatementBuilder() {
+        return new SolidSqlStatementBuilder();
+    }
+
+    @Override
+    public String getDbName() {
+        return "solid";
+    }
+
+    @Override
+    protected String doGetCurrentSchema() throws SQLException {
+        return jdbcTemplate.queryForString("SELECT CURRENT_SCHEMA()");
+    }
+
+    @Override
+    protected void doSetCurrentSchema(final Schema schema) throws SQLException {
+        jdbcTemplate.execute("SET SCHEMA " + schema);
+    }
+
+    @Override
+    public String getCurrentUserFunction() {
+        return "NAME FROM _SYSTEM.SYS_USERS WHERE ID = LOGIN_USERID()";
+    }
+
+    @Override
+    public boolean supportsDdlTransactions() {
+        return true;
+    }
+
+    @Override
+    public String getBooleanTrue() {
+        return "1";
+    }
+
+    @Override
+    public String getBooleanFalse() {
+        return "0";
+    }
+
+    @Override
+    protected String doQuote(final String identifier) {
+        return "'" + identifier + "'";
+    }
+
+    @Override
+    public boolean catalogIsSchema() {
+        return false;
+    }
+}
---
+++ flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/solid/SolidSqlStatementBuilder.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2010-2014 Axel Fontaine
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//
+// Project: spring-boot-sample-flyway
+//
+// Media-Saturn IT Services GmbH
+//
+// Wankelstr. 5
+// 85046 Ingolstadt
+// Telefon: +49 (841) 634-0
+// Telefax: +49 (841) 634-992596
+// Web:     www.media-saturn.com
+//
+
+package org.flywaydb.core.internal.dbsupport.solid;
+
+import org.flywaydb.core.internal.dbsupport.SqlStatementBuilder;
+
+public class SolidSqlStatementBuilder extends SqlStatementBuilder {
+    // TODO: override methods if necessary
+}
