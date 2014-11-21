7dad6fb77e73f3dbad522131c9ca8b9b9febb8be
Axel Fontaine <business@axelfontaine.com>
2014-11-21 18:53:49 +0100

    Fixed broken test

--- flyway-core/src/test/java/org/flywaydb/core/internal/dbsupport/db2/DB2SchemaSmallTest.java
+++ flyway-core/src/test/java/org/flywaydb/core/internal/dbsupport/db2/DB2SchemaSmallTest.java
@@ -79,7 +79,7 @@ public class DB2SchemaSmallTest {
         // Return a VERSIONED_TABLE when the SQL to find version tables is called.
         List<String> versionedTables = new ArrayList<String>();
         versionedTables.add("VERSIONED_TABLE");
-        when(jdbcTemplate.queryForStringList("select rtrim(TABNAME) from SYSCAT.TABLES where TEMPORALTYPE <> 'N' and TABSCHEMA = ?", "SCHEMA")).thenReturn(versionedTables);
+        when(jdbcTemplate.queryForStringList("select TABNAME from SYSCAT.TABLES where TEMPORALTYPE <> 'N' and TABSCHEMA = ?", "SCHEMA")).thenReturn(versionedTables);
 
         when(dbSupport.getDb2MajorVersion()).thenReturn(10);
         when(dbSupport.quote("SCHEMA", "VERSIONED_TABLE")).thenReturn("SCHEMA.VERSIONED_TABLE");
