67e4ab35babd6ab54638df8226f8d8e6fd398677
Axel Fontaine <business@axelfontaine.com>
2014-11-24 15:28:51 +0100

    Fixed #824 Classpath scanner does not detect sql-files in WEB-INF/classes in (non-exploded) war-file

--- flyway-core/src/main/java/org/flywaydb/core/internal/util/scanner/classpath/JarFileClassPathLocationScanner.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/util/scanner/classpath/JarFileClassPathLocationScanner.java
@@ -34,7 +34,9 @@ public class JarFileClassPathLocationScanner implements ClassPathLocationScanner
         JarFile jarFile = getJarFromUrl(locationUrl);
 
         try {
-            return findResourceNamesFromJarFile(jarFile, location);
+            // For Tomcat and non-expanded WARs.
+            String prefix = jarFile.getName().toLowerCase().endsWith(".war") ? "WEB-INF/classes/" : "";
+            return findResourceNamesFromJarFile(jarFile, prefix, location);
         } finally {
             jarFile.close();
         }
@@ -83,19 +85,20 @@ public class JarFileClassPathLocationScanner implements ClassPathLocationScanner
      * Finds all the resource names contained in this directory within this jar file.
      *
      * @param jarFile  The jar file.
+     * @param prefix   The prefix to ignore within the jar file.
      * @param location The location to look under.
      * @return The resource names.
      * @throws java.io.IOException when reading the jar file failed.
      */
-    private Set<String> findResourceNamesFromJarFile(JarFile jarFile, String location) throws IOException {
-        location += location.endsWith("/") ? "" : "/";
+    private Set<String> findResourceNamesFromJarFile(JarFile jarFile, String prefix, String location) throws IOException {
+        String toScan = prefix + location + (location.endsWith("/") ? "" : "/");
         Set<String> resourceNames = new TreeSet<String>();
 
         Enumeration<JarEntry> entries = jarFile.entries();
         while (entries.hasMoreElements()) {
             String entryName = entries.nextElement().getName();
-            if (entryName.startsWith(location)) {
-                resourceNames.add(entryName);
+            if (entryName.startsWith(toScan)) {
+                resourceNames.add(entryName.substring(prefix.length()));
             }
         }
 
