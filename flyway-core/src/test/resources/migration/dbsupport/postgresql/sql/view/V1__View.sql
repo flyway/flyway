1dae5241424dbecba6f49019eedde9a451e1a082
Axel Fontaine <business@axelfontaine.com>
2015-03-05 14:55:51 +0100

    Fixed #918 Explicitly drop Views then doing a Flyway.clean() on a PostgreSQL db

--- flyway-core/src/test/resources/migration/dbsupport/postgresql/sql/view/V1__View.sql
+++ flyway-core/src/test/resources/migration/dbsupport/postgresql/sql/view/V1__View.sql
@@ -17,3 +17,5 @@
 CREATE TABLE """t""" (qty INT, price INT);
 INSERT INTO """t""" VALUES(3, 50);
 CREATE VIEW """v""" AS SELECT qty, price, qty*price AS value FROM """t""";
+
+CREATE VIEW features AS SELECT * FROM information_schema.sql_features;
