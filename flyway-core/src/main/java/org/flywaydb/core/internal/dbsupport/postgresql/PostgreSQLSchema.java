2cd038e7ebb0c7318265b9049a9ad6d7ada72bab
Aurélien Mino <aurelien.mino@gmail.com>
2016-02-01 22:12:07 +0100

    PostgreSQL: Fixed cleaning after the drop of final all types dropping. - Some custom types where not retrieved in generateDropStatementsForBaseTypes(). This method has been improved to find more types, however not all types should be recreated. - generateDropStatementsForViews() has been rewritten to correctly deal with views with quotes

--- flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/postgresql/PostgreSQLSchema.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/postgresql/PostgreSQLSchema.java
@@ -22,6 +22,7 @@ import org.flywaydb.core.internal.dbsupport.Type;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -141,19 +142,26 @@ public class PostgreSQLSchema extends Schema<PostgreSQLDbSupport> {
      * @throws SQLException when the clean statements could not be generated.
      */
     private List<String> generateDropStatementsForBaseTypes(boolean recreate) throws SQLException {
-        List<String> typeNames =
-                jdbcTemplate.queryForStringList(
-                "select typname from pg_catalog.pg_type where typcategory in ('P', 'U') and typnamespace in (select oid from pg_catalog.pg_namespace where nspname = ?)",
+        
+        List<Map<String, String>> rows =
+                jdbcTemplate.queryForList(
+                "select typname, typcategory from pg_catalog.pg_type t "
+                + "where (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid)) and "
+                + "NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid) and "
+                + "t.typnamespace in (select oid from pg_catalog.pg_namespace where nspname = ?)",
                 name);
 
         List<String> statements = new ArrayList<String>();
-        for (String typeName : typeNames) {
-            statements.add("DROP TYPE IF EXISTS " + dbSupport.quote(name, typeName) + " CASCADE");
+        for (Map<String, String> row : rows) {
+            statements.add("DROP TYPE IF EXISTS " + dbSupport.quote(name, row.get("typname")) + " CASCADE");
         }
 
         if (recreate) {
-            for (String typeName : typeNames) {
-                statements.add("CREATE TYPE " + dbSupport.quote(name, typeName));
+            for (Map<String, String> row : rows) {
+                // Only recreate Pseudo-types (P) and User-defined types (U)
+                if (Arrays.asList("P", "U").contains(row.get("typcategory"))) {
+                    statements.add("CREATE TYPE " + dbSupport.quote(name, row.get("typname")));
+                }
             }
         }
 
@@ -275,10 +283,10 @@ public class PostgreSQLSchema extends Schema<PostgreSQLDbSupport> {
         List<String> viewNames =
                 jdbcTemplate.queryForStringList(
                 // Search for all views
-                "SELECT table_name FROM information_schema.views" +
+                "SELECT relname FROM pg_catalog.pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace" +
                         // that don't depend on an extension
-                        " LEFT JOIN pg_depend dep ON dep.objid = (table_schema || '.' || table_name)::regclass::oid AND dep.deptype = 'e'" +
-                        " WHERE table_schema=? AND dep.objid IS NULL",
+                        " LEFT JOIN pg_depend dep ON dep.objid = c.oid AND dep.deptype = 'e'" +
+                        " WHERE c.relkind = 'v' AND  n.nspname = ? AND dep.objid IS NULL",
                 name);
         List<String> statements = new ArrayList<String>();
         for (String domainName : viewNames) {
