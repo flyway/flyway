299b54da2f433c1b32ab64bf734d8da373d0790c
steveohara1 <steve.m.ohara@gmail.com>
2016-02-17 09:56:05 +0000

    reformatted code to intelliJ standard

--- flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/postgresql/PostgreSQLDbSupport.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/postgresql/PostgreSQLDbSupport.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2010-2016 Boxfuse GmbH
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -120,13 +120,13 @@ public class PostgreSQLDbSupport extends DbSupport {
     }
 
     public String unQuote(final String quoted) {
-    	if (quoted==null||quoted.length()<=2) {
-    		return quoted;
-    	}
-    	String result = quoted.startsWith("\"")?quoted.substring(1):quoted;
-    	return result.endsWith("\"")?result.substring(0,result.length()-1):result;
+        if (quoted == null || quoted.length() <= 2) {
+            return quoted;
+        }
+        String result = quoted.startsWith("\"") ? quoted.substring(1) : quoted;
+        return result.endsWith("\"") ? result.substring(0, result.length() - 1) : result;
     }
-    
+
     @Override
     public Schema getSchema(String name) {
         return new PostgreSQLSchema(jdbcTemplate, this, name);
