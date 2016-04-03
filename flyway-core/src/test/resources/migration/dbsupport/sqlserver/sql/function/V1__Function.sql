b161c9ab172fbb97565ed3ef45e41d8f0122b43d
Jean Revertera <jean.revertera@gmail.com>
2016-04-03 14:09:57 +0200

    Tests: inlined + table-valued functions

--- flyway-core/src/test/resources/migration/dbsupport/sqlserver/sql/function/V1__Function.sql
+++ flyway-core/src/test/resources/migration/dbsupport/sqlserver/sql/function/V1__Function.sql
@@ -42,3 +42,35 @@ GO
 
 ALTER TABLE [dbo].[EventStream] ADD  CONSTRAINT [DF_EventStream_EventDate]  DEFAULT (getdate()) FOR [EventDate]
 GO
+
+CREATE TABLE test_data (
+  value VARCHAR(25) NOT NULL,
+  PRIMARY KEY(value)
+)
+GO
+
+/* Inline user-defined function. */
+CREATE function reverseInlineFunc()
+RETURNS TABLE
+AS
+RETURN (
+	SELECT REVERSE(value) AS value FROM test_data
+)
+GO
+
+/* Table-valued function. Filled with strictly positive integers (starting at 1) up to a upper bound. */
+CREATE FUNCTION positiveIntegers(@to int)
+RETURNS @oneToTen TABLE (
+	number int NOT NULL
+)
+AS
+BEGIN
+	DECLARE @cnt INT = 1
+	WHILE @cnt <= @to
+	BEGIN
+	   INSERT INTO @oneToTen VALUES (@cnt)
+	   SET @cnt = @cnt + 1
+	END
+	RETURN
+END
+GO
