0cae7ca608d36b61a39d3fdf4f2266cb2895adea
Jean Revertera <jean.revertera@gmail.com>
2016-04-05 14:28:35 +0200

    Added missing type selection in doEmpty()

--- flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/sqlserver/SQLServerSchema.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/sqlserver/SQLServerSchema.java
@@ -146,7 +146,7 @@ public class SQLServerSchema extends Schema<SQLServerDbSupport> {
     protected boolean doEmpty() throws SQLException {
 
 
-        boolean empty  = queryDBObjects(ObjectType.SCALAR_FUNCTION,
+        boolean empty  = queryDBObjects(ObjectType.SCALAR_FUNCTION, ObjectType.AGGREGATE,
                 ObjectType.CLR_SCALAR_FUNCTION, ObjectType.CLR_TABLE_VALUED_FUNCTION, ObjectType.TABLE_VALUED_FUNCTION,
                 ObjectType.STORED_PROCEDURE, ObjectType.CLR_STORED_PROCEDURE, ObjectType.USER_TABLE,
                 ObjectType.SYNONYM, ObjectType.SEQUENCE_OBJECT, ObjectType.FOREIGN_KEY, ObjectType.VIEW).isEmpty();
