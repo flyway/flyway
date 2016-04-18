a5736d0b5098f0545240ff8b9cd0a843558799ad
azalesky <azalesky@by.byelex.com>
2016-04-18 16:31:01 +0300

    #1265 fix upgrade metadata table for Derby. Drop not null constraint with Derby syntax. (#1269)

--- flyway-core/src/main/resources/org/flywaydb/core/internal/dbsupport/derby/upgradeMetaDataTable.sql
+++ flyway-core/src/main/resources/org/flywaydb/core/internal/dbsupport/derby/upgradeMetaDataTable.sql
@@ -18,6 +18,6 @@ DROP INDEX "${schema}"."${table}_vr_idx";
 DROP INDEX "${schema}"."${table}_ir_idx";
 ALTER TABLE "${schema}"."${table}" DROP COLUMN "version_rank";
 ALTER TABLE "${schema}"."${table}" DROP CONSTRAINT "${table}_pk";
-ALTER TABLE "${schema}"."${table}" ALTER COLUMN "version" DROP NOT NULL;
+ALTER TABLE "${schema}"."${table}" ALTER COLUMN "version" NULL;
 ALTER TABLE "${schema}"."${table}" ADD CONSTRAINT "${table}_pk" PRIMARY KEY ("installed_rank");
 UPDATE "${schema}"."${table}" SET "type"='BASELINE' WHERE "type"='INIT';
