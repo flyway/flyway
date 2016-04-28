flyway/flyway

commit cbc9fef79c67db7df4e82fa8604b510ef059094d

Fixed #979 can't migrate as long as there are compile errors

flyway-core/src/main/java/org/flywaydb/core/internal/util/scanner/classpath/ClassPathScanner.java
@@ -18,10 +18,11 @@ package org.flywaydb.core.internal.util.scanner.classpath;
 import org.flywaydb.core.api.FlywayException;
 import org.flywaydb.core.internal.util.ClassUtils;
 import org.flywaydb.core.internal.util.FeatureDetector;
-import org.flywaydb.core.internal.util.scanner.Resource;
+import org.flywaydb.core.internal.util.Location;
 import org.flywaydb.core.internal.util.UrlUtils;
 import org.flywaydb.core.internal.util.logging.Log;
 import org.flywaydb.core.internal.util.logging.LogFactory;
+import org.flywaydb.core.internal.util.scanner.Resource;
 import org.flywaydb.core.internal.util.scanner.classpath.jboss.JBossVFSv2UrlResolver;
 import org.flywaydb.core.internal.util.scanner.classpath.jboss.JBossVFSv3ClassPathLocationScanner;
 
@@ -31,20 +32,37 @@ import java.net.URL;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 /**
  * ClassPath scanner.
  */
-public class ClassPathScanner {
+public class ClassPathScanner implements ResourceAndClassScanner {
     private static final Log LOG = LogFactory.getLog(ClassPathScanner.class);
 
     /**
      * The ClassLoader for loading migrations on the classpath.
      */
     private final ClassLoader classLoader;
+
+    /**
+     * Cache location lookups.
+     */
+    private final Map<Location, List<URL>> locationUrlCache = new HashMap<Location, List<URL>>();
+
+    /**
+     * Cache location scanners.
+     */
+    private final Map<String, ClassPathLocationScanner> locationScannerCache = new HashMap<String, ClassPathLocationScanner>();
+
+    /**
+     * Cache resource names.
+     */
+    private final Map<ClassPathLocationScanner, Map<URL, Set<String>>> resourceNameCache = new HashMap<ClassPathLocationScanner, Map<URL, Set<String>>>();
 
     /**
      * Creates a new Classpath scanner.
@@ -55,17 +73,8 @@ public class ClassPathScanner {
         this.classLoader = classLoader;
     }
 
-    /**
-     * Scans the classpath for resources under the specified location, starting with the specified prefix and ending with
-     * the specified suffix.
-     *
-     * @param path   The path in the classpath to start searching. Subdirectories are also searched.
-     * @param prefix The prefix of the resource names to match.
-     * @param suffix The suffix of the resource names to match.
-     * @return The resources that were found.
-     * @throws IOException when the location could not be scanned.
-     */
-    public Resource[] scanForResources(String path, String prefix, String suffix) throws IOException {
+    @Override
+    public Resource[] scanForResources(Location path, String prefix, String suffix) throws IOException {
         LOG.debug("Scanning for classpath resources at '" + path + "' (Prefix: '" + prefix + "', Suffix: '" + suffix + "')");
 
         Set<Resource> resources = new TreeSet<Resource>();
@@ -79,17 +88,8 @@ public class ClassPathScanner {
         return resources.toArray(new Resource[resources.size()]);
     }
 
-    /**
-     * Scans the classpath for concrete classes under the specified package implementing this interface.
-     * Non-instantiable abstract classes are filtered out.
-     *
-     * @param location             The location (package) in the classpath to start scanning.
-     *                             Subpackages are also scanned.
-     * @param implementedInterface The interface the matching classes should implement.
-     * @return The non-abstract classes that were found.
-     * @throws Exception when the location could not be scanned.
-     */
-    public Class<?>[] scanForClasses(String location, Class<?> implementedInterface) throws Exception {
+    @Override
+    public Class<?>[] scanForClasses(Location location, Class<?> implementedInterface) throws Exception {
         LOG.debug("Scanning for classes at '" + location + "' (Implementing: '" + implementedInterface.getName() + "')");
 
         List<Class<?>> classes = new ArrayList<Class<?>>();
@@ -136,16 +136,16 @@ public class ClassPathScanner {
      * Finds the resources names present at this location and below on the classpath starting with this prefix and
      * ending with this suffix.
      *
-     * @param path   The path on the classpath to scan.
-     * @param prefix The filename prefix to match.
-     * @param suffix The filename suffix to match.
+     * @param location The location on the classpath to scan.
+     * @param prefix   The filename prefix to match.
+     * @param suffix   The filename suffix to match.
      * @return The resource names.
      * @throws IOException when scanning this location failed.
      */
-    private Set<String> findResourceNames(String path, String prefix, String suffix) throws IOException {
+    private Set<String> findResourceNames(Location location, String prefix, String suffix) throws IOException {
         Set<String> resourceNames = new TreeSet<String>();
 
-        List<URL> locationsUrls = getLocationUrlsForPath(path);
+        List<URL> locationsUrls = getLocationUrlsForPath(location);
         for (URL locationUrl : locationsUrls) {
             LOG.debug("Scanning URL: " + locationUrl.toExternalForm());
 
@@ -158,7 +158,12 @@ public class ClassPathScanner {
                 String scanRoot = UrlUtils.toFilePath(resolvedUrl);
                 LOG.warn("Unable to scan location: " + scanRoot + " (unsupported protocol: " + protocol + ")");
             } else {
-                resourceNames.addAll(classPathLocationScanner.findResourceNames(path, resolvedUrl));
+                Set<String> names = resourceNameCache.get(classPathLocationScanner).get(resolvedUrl);
+                if (names == null) {
+                    names = classPathLocationScanner.findResourceNames(location.getPath(), resolvedUrl);
+                    resourceNameCache.get(classPathLocationScanner).put(resolvedUrl, names);
+                }
+                resourceNames.addAll(names);
             }
         }
 
@@ -168,18 +173,24 @@ public class ClassPathScanner {
     /**
      * Gets the physical location urls for this logical path on the classpath.
      *
-     * @param path The path on the classpath.
+     * @param location The location on the classpath.
      * @return The underlying physical URLs.
      * @throws IOException when the lookup fails.
      */
-    private List<URL> getLocationUrlsForPath(String path) throws IOException {
+    private List<URL> getLocationUrlsForPath(Location location) throws IOException {
+        if (locationUrlCache.containsKey(location)) {
+            return locationUrlCache.get(location);
+        }
+
+        LOG.debug("Determining location urls for " + location + " using ClassLoader " + classLoader + " ...");
+
         List<URL> locationUrls = new ArrayList<URL>();
 
         if (classLoader.getClass().getName().startsWith("com.ibm")) {
             // WebSphere
-            Enumeration<URL> urls = classLoader.getResources(path + "/flyway.location");
+            Enumeration<URL> urls = classLoader.getResources(location + "/flyway.location");
             if (!urls.hasMoreElements()) {
-                LOG.warn("Unable to determine URL for classpath location: " + path + " (ClassLoader: " + classLoader + ")"
+                LOG.warn("Unable to resolve location " + location + " (ClassLoader: " + classLoader + ")"
                         + " On WebSphere an empty file named flyway.location must be present on the classpath location for WebSphere to find it!");
             }
             while (urls.hasMoreElements()) {
@@ -187,15 +198,17 @@ public class ClassPathScanner {
                 locationUrls.add(new URL(URLDecoder.decode(url.toExternalForm(), "UTF-8").replace("/flyway.location", "")));
             }
         } else {
-            Enumeration<URL> urls = classLoader.getResources(path);
+            Enumeration<URL> urls = classLoader.getResources(location.getPath());
             if (!urls.hasMoreElements()) {
-                LOG.warn("Unable to determine URL for classpath location: " + path + " (ClassLoader: " + classLoader + ")");
+                LOG.warn("Unable to resolve location " + location);
             }
 
             while (urls.hasMoreElements()) {
                 locationUrls.add(urls.nextElement());
             }
         }
+
+        locationUrlCache.put(location, locationUrls);
 
         return locationUrls;
     }
@@ -221,26 +234,42 @@ public class ClassPathScanner {
      * @return The location scanner or {@code null} if it could not be created.
      */
     private ClassPathLocationScanner createLocationScanner(String protocol) {
+        if (locationScannerCache.containsKey(protocol)) {
+            return locationScannerCache.get(protocol);
+        }
+
         if ("file".equals(protocol)) {
-            return new FileSystemClassPathLocationScanner();
+            FileSystemClassPathLocationScanner locationScanner = new FileSystemClassPathLocationScanner();
+            locationScannerCache.put(protocol, locationScanner);
+            resourceNameCache.put(locationScanner, new HashMap<URL, Set<String>>());
+            return locationScanner;
         }
 
         if ("jar".equals(protocol)
                 || "zip".equals(protocol) //WebLogic
                 || "wsjar".equals(protocol) //WebSphere
                 ) {
-            return new JarFileClassPathLocationScanner();
+            JarFileClassPathLocationScanner locationScanner = new JarFileClassPathLocationScanner();
+            locationScannerCache.put(protocol, locationScanner);
+            resourceNameCache.put(locationScanner, new HashMap<URL, Set<String>>());
+            return locationScanner;
         }
 
         FeatureDetector featureDetector = new FeatureDetector(classLoader);
         if (featureDetector.isJBossVFSv3Available() && "vfs".equals(protocol)) {
-            return new JBossVFSv3ClassPathLocationScanner();
+            JBossVFSv3ClassPathLocationScanner locationScanner = new JBossVFSv3ClassPathLocationScanner();
+            locationScannerCache.put(protocol, locationScanner);
+            resourceNameCache.put(locationScanner, new HashMap<URL, Set<String>>());
+            return locationScanner;
         }
         if (featureDetector.isOsgiFrameworkAvailable() && (
                 "bundle".equals(protocol) // Felix
                         || "bundleresource".equals(protocol)) //Equinox
                 ) {
-            return new OsgiClassPathLocationScanner();
+            OsgiClassPathLocationScanner locationScanner = new OsgiClassPathLocationScanner();
+            locationScannerCache.put(protocol, locationScanner);
+            resourceNameCache.put(locationScanner, new HashMap<URL, Set<String>>());
+            return locationScanner;
         }
 
         return null;

flyway-commandline/src/test/java/org/flywaydb/commandline/MainClassLoaderSmallTest.java
@@ -15,6 +15,7 @@
  */
 package org.flywaydb.commandline;
 
+import org.flywaydb.core.internal.util.Location;
 import org.flywaydb.core.internal.util.logging.console.ConsoleLog.Level;
 import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
 import org.flywaydb.core.internal.util.scanner.classpath.ClassPathResource;
@@ -93,7 +94,7 @@ public class MainClassLoaderSmallTest {
 
         assertTrue(new ClassPathResource("pkg/runtime.conf", getClassLoader()).exists());
 
-        Resource[] resources = new ClassPathScanner(getClassLoader()).scanForResources("pkg", "run", ".conf");
+        Resource[] resources = new ClassPathScanner(getClassLoader()).scanForResources(new Location("classpath:pkg"), "run", ".conf");
         assertEquals("pkg/runtime.conf", resources[0].getLocation());
     }
 
@@ -109,7 +110,7 @@ public class MainClassLoaderSmallTest {
 
         assertTrue(new ClassPathResource("funtime.properties", getClassLoader()).exists());
 
-        Resource[] resources = new ClassPathScanner(getClassLoader()).scanForResources("", "fun", ".properties");
+        Resource[] resources = new ClassPathScanner(getClassLoader()).scanForResources(new Location("classpath:"), "fun", ".properties");
         assertEquals("funtime.properties", resources[1].getLocation());
     }
 
@@ -132,10 +133,10 @@ public class MainClassLoaderSmallTest {
         assertTrue(new ClassPathResource("db/migration/V1__Initial_structure.sql", getClassLoader()).exists());
         assertTrue(ClassUtils.isPresent("org.flywaydb.sample.migration.V1_2__Another_user", getClassLoader()));
 
-        Resource[] resources = new ClassPathScanner(getClassLoader()).scanForResources("db/migration", "V1__", ".sql");
+        Resource[] resources = new ClassPathScanner(getClassLoader()).scanForResources(new Location("classpath:db/migration"), "V1__", ".sql");
         assertEquals("db/migration/V1__Initial_structure.sql", resources[0].getLocation());
 
-        Class<?>[] classes = new ClassPathScanner(getClassLoader()).scanForClasses("org/flywaydb/sample/migration", SpringJdbcMigration.class);
+        Class<?>[] classes = new ClassPathScanner(getClassLoader()).scanForClasses(new Location("classpath:org/flywaydb/sample/migration"), SpringJdbcMigration.class);
         assertEquals("org.flywaydb.sample.migration.V1_2__Another_user", classes[0].getName());
     }
 }

flyway-core/src/main/java/org/flywaydb/core/Flyway.java
@@ -46,6 +46,7 @@ import org.flywaydb.core.internal.util.jdbc.TransactionCallback;
 import org.flywaydb.core.internal.util.jdbc.TransactionTemplate;
 import org.flywaydb.core.internal.util.logging.Log;
 import org.flywaydb.core.internal.util.logging.LogFactory;
+import org.flywaydb.core.internal.util.scanner.Scanner;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -855,10 +856,9 @@ public class Flyway {
      */
     public int migrate() throws FlywayException {
         return execute(new Command<Integer>() {
-            public Integer execute(Connection connectionMetaDataTable, Connection connectionUserObjects, DbSupport dbSupport, Schema[] schemas) {
+            public Integer execute(Connection connectionMetaDataTable, Connection connectionUserObjects, MigrationResolver migrationResolver, DbSupport dbSupport, Schema[] schemas) {
                 MetaDataTable metaDataTable = new MetaDataTableImpl(dbSupport, schemas[0].getTable(table));
 
-                MigrationResolver migrationResolver = createMigrationResolver(dbSupport);
                 if (validateOnMigrate) {
                     doValidate(connectionMetaDataTable, dbSupport, migrationResolver, metaDataTable, schemas, true);
                 }
@@ -911,9 +911,8 @@ public class Flyway {
      */
     public void validate() throws FlywayException {
         execute(new Command<Void>() {
-            public Void execute(Connection connectionMetaDataTable, Connection connectionUserObjects, DbSupport dbSupport, Schema[] schemas) {
+            public Void execute(Connection connectionMetaDataTable, Connection connectionUserObjects, MigrationResolver migrationResolver, DbSupport dbSupport, Schema[] schemas) {
                 MetaDataTable metaDataTable = new MetaDataTableImpl(dbSupport, schemas[0].getTable(table));
-                MigrationResolver migrationResolver = createMigrationResolver(dbSupport);
 
                 doValidate(connectionMetaDataTable, dbSupport, migrationResolver, metaDataTable, schemas, false);
                 return null;
@@ -955,7 +954,7 @@ public class Flyway {
      */
     public void clean() {
         execute(new Command<Void>() {
-            public Void execute(Connection connectionMetaDataTable, Connection connectionUserObjects, DbSupport dbSupport, Schema[] schemas) {
+            public Void execute(Connection connectionMetaDataTable, Connection connectionUserObjects, MigrationResolver migrationResolver, DbSupport dbSupport, Schema[] schemas) {
                 MetaDataTableImpl metaDataTable =
                         new MetaDataTableImpl(dbSupport, schemas[0].getTable(table));
                 new DbClean(connectionMetaDataTable, dbSupport, metaDataTable, schemas, callbacks).clean();
@@ -975,7 +974,7 @@ public class Flyway {
     public MigrationInfoService info() {
         return execute(new Command<MigrationInfoService>() {
             public MigrationInfoService execute(final Connection connectionMetaDataTable, Connection connectionUserObjects,
-                                                final DbSupport dbSupport, final Schema[] schemas) {
+                                                MigrationResolver migrationResolver, final DbSupport dbSupport, final Schema[] schemas) {
                 try {
                     for (final FlywayCallback callback : getCallbacks()) {
                         new TransactionTemplate(connectionMetaDataTable).execute(new TransactionCallback<Object>() {
@@ -989,7 +988,6 @@ public class Flyway {
                     }
 
                     dbSupport.changeCurrentSchemaTo(schemas[0]);
-                    MigrationResolver migrationResolver = createMigrationResolver(dbSupport);
                     MetaDataTable metaDataTable = new MetaDataTableImpl(dbSupport, schemas[0].getTable(table));
 
                     MigrationInfoServiceImpl migrationInfoService =
@@ -1024,7 +1022,7 @@ public class Flyway {
      */
     public void baseline() throws FlywayException {
         execute(new Command<Void>() {
-            public Void execute(Connection connectionMetaDataTable, Connection connectionUserObjects, DbSupport dbSupport, Schema[] schemas) {
+            public Void execute(Connection connectionMetaDataTable, Connection connectionUserObjects, MigrationResolver migrationResolver, DbSupport dbSupport, Schema[] schemas) {
                 MetaDataTable metaDataTable = new MetaDataTableImpl(dbSupport, schemas[0].getTable(table));
                 new DbSchemas(connectionMetaDataTable, schemas, metaDataTable).create();
                 new DbBaseline(connectionMetaDataTable, dbSupport, metaDataTable, schemas[0], baselineVersion, baselineDescription, callbacks).baseline();
@@ -1045,8 +1043,7 @@ public class Flyway {
      */
     public void repair() throws FlywayException {
         execute(new Command<Void>() {
-            public Void execute(Connection connectionMetaDataTable, Connection connectionUserObjects, DbSupport dbSupport, Schema[] schemas) {
-                MigrationResolver migrationResolver = createMigrationResolver(dbSupport);
+            public Void execute(Connection connectionMetaDataTable, Connection connectionUserObjects, MigrationResolver migrationResolver, DbSupport dbSupport, Schema[] schemas) {
                 MetaDataTable metaDataTable = new MetaDataTableImpl(dbSupport, schemas[0].getTable(table));
                 new DbRepair(dbSupport, connectionMetaDataTable, schemas[0], migrationResolver, metaDataTable, callbacks).repair();
                 return null;
@@ -1058,10 +1055,11 @@ public class Flyway {
      * Creates the MigrationResolver.
      *
      * @param dbSupport The database-specific support.
+     * @param scanner   The Scanner for resolving migrations.
      * @return A new, fully configured, MigrationResolver instance.
      */
-    private MigrationResolver createMigrationResolver(DbSupport dbSupport) {
-        return new CompositeMigrationResolver(dbSupport, classLoader, locations,
+    private MigrationResolver createMigrationResolver(DbSupport dbSupport, Scanner scanner) {
+        return new CompositeMigrationResolver(dbSupport, scanner, locations,
                 encoding, sqlMigrationPrefix, sqlMigrationSeparator, sqlMigrationSuffix, createPlaceholderReplacer(),
                 resolvers);
     }
@@ -1206,6 +1204,7 @@ public class Flyway {
 
     /**
      * Retrieves the value for this key in this map and removes the corresponding entry from the map.
+     *
      * @param map The map.
      * @param key The key.
      * @return The value. {@code null} if not found.
@@ -1265,13 +1264,16 @@ public class Flyway {
                 schemas[i] = dbSupport.getSchema(schemaNames[i]);
             }
 
+            Scanner scanner = new Scanner(classLoader);
+            MigrationResolver migrationResolver = createMigrationResolver(dbSupport, scanner);
+
             if (callbacks.length == 0) {
-                setCallbacks(new SqlScriptFlywayCallback(dbSupport, classLoader, locations, createPlaceholderReplacer(),
+                setCallbacks(new SqlScriptFlywayCallback(dbSupport, scanner, locations, createPlaceholderReplacer(),
                         encoding, sqlMigrationSuffix));
                 callbackAutoAdded = true;
             }
 
-            result = command.execute(connectionMetaDataTable, connectionUserObjects, dbSupport, schemas);
+            result = command.execute(connectionMetaDataTable, connectionUserObjects, migrationResolver, dbSupport, schemas);
         } finally {
             if (callbackAutoAdded) {
                 setCallbacksAsClassNames();
@@ -1298,10 +1300,10 @@ public class Flyway {
          *
          * @param connectionMetaDataTable The database connection for the metadata table changes.
          * @param connectionUserObjects   The database connection for user object changes.
-         * @param dbSupport               The database-specific support for these connections.
-         * @param schemas                 The schemas managed by Flyway.
-         * @return The result of the operation.
+         * @param migrationResolver
+         *@param dbSupport               The database-specific support for these connections.
+         * @param schemas                 The schemas managed by Flyway.   @return The result of the operation.
          */
-        T execute(Connection connectionMetaDataTable, Connection connectionUserObjects, DbSupport dbSupport, Schema[] schemas);
+        T execute(Connection connectionMetaDataTable, Connection connectionUserObjects, MigrationResolver migrationResolver, DbSupport dbSupport, Schema[] schemas);
     }
 }

flyway-core/src/main/java/org/flywaydb/core/internal/callback/SqlScriptFlywayCallback.java
@@ -45,16 +45,14 @@ public class SqlScriptFlywayCallback implements FlywayCallback {
      * Creates a new instance.
      *
      * @param dbSupport           The database-specific support.
-     * @param classLoader         The ClassLoader for loading migrations on the classpath.
+     * @param scanner             The Scanner for loading migrations on the classpath.
      * @param locations           The locations where migrations are located.
      * @param placeholderReplacer The placeholder replacer to apply to sql migration scripts.
      * @param encoding            The encoding of Sql migrations.
      * @param sqlMigrationSuffix  The suffix for sql migrations
      */
-    public SqlScriptFlywayCallback(DbSupport dbSupport, ClassLoader classLoader, Locations locations,
+    public SqlScriptFlywayCallback(DbSupport dbSupport, Scanner scanner, Locations locations,
                                    PlaceholderReplacer placeholderReplacer, String encoding, String sqlMigrationSuffix) {
-        Scanner scanner = new Scanner(classLoader);
-
         scripts.put("beforeClean", null);
         scripts.put("afterClean", null);
         scripts.put("beforeMigrate", null);

flyway-core/src/main/java/org/flywaydb/core/internal/resolver/CompositeMigrationResolver.java
@@ -26,6 +26,7 @@ import org.flywaydb.core.internal.util.FeatureDetector;
 import org.flywaydb.core.internal.util.Location;
 import org.flywaydb.core.internal.util.Locations;
 import org.flywaydb.core.internal.util.PlaceholderReplacer;
+import org.flywaydb.core.internal.util.scanner.Scanner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -55,7 +56,7 @@ public class CompositeMigrationResolver implements MigrationResolver {
      * Creates a new CompositeMigrationResolver.
      *
      * @param dbSupport                The database-specific support.
-     * @param classLoader              The ClassLoader for loading migrations on the classpath.
+     * @param scanner                  The Scanner for loading migrations on the classpath.
      * @param locations                The locations where migrations are located.
      * @param encoding                 The encoding of Sql migrations.
      * @param sqlMigrationPrefix       The file name prefix for sql migrations.
@@ -64,18 +65,18 @@ public class CompositeMigrationResolver implements MigrationResolver {
      * @param placeholderReplacer      The placeholder replacer to use.
      * @param customMigrationResolvers Custom Migration Resolvers.
      */
-    public CompositeMigrationResolver(DbSupport dbSupport, ClassLoader classLoader, Locations locations,
+    public CompositeMigrationResolver(DbSupport dbSupport, Scanner scanner, Locations locations,
                                       String encoding,
                                       String sqlMigrationPrefix, String sqlMigrationSeparator, String sqlMigrationSuffix,
                                       PlaceholderReplacer placeholderReplacer,
                                       MigrationResolver... customMigrationResolvers) {
         for (Location location : locations.getLocations()) {
-            migrationResolvers.add(new SqlMigrationResolver(dbSupport, classLoader, location, placeholderReplacer,
+            migrationResolvers.add(new SqlMigrationResolver(dbSupport, scanner, location, placeholderReplacer,
                     encoding, sqlMigrationPrefix, sqlMigrationSeparator, sqlMigrationSuffix));
-            migrationResolvers.add(new JdbcMigrationResolver(classLoader, location));
+            migrationResolvers.add(new JdbcMigrationResolver(scanner, location));
 
-            if (new FeatureDetector(classLoader).isSpringJdbcAvailable()) {
-                migrationResolvers.add(new SpringJdbcMigrationResolver(classLoader, location));
+            if (new FeatureDetector(scanner.getClassLoader()).isSpringJdbcAvailable()) {
+                migrationResolvers.add(new SpringJdbcMigrationResolver(scanner, location));
             }
         }
 

flyway-core/src/main/java/org/flywaydb/core/internal/resolver/ResolvedMigrationImpl.java
@@ -159,8 +159,6 @@ public class ResolvedMigrationImpl implements ResolvedMigration {
         if (checksum != null ? !checksum.equals(migration.checksum) : migration.checksum != null) return false;
         if (description != null ? !description.equals(migration.description) : migration.description != null)
             return false;
-        if (physicalLocation != null ? !physicalLocation.equals(migration.physicalLocation) : migration.physicalLocation != null)
-            return false;
         if (script != null ? !script.equals(migration.script) : migration.script != null) return false;
         if (type != migration.type) return false;
         return version.equals(migration.version);
@@ -173,7 +171,6 @@ public class ResolvedMigrationImpl implements ResolvedMigration {
         result = 31 * result + (script != null ? script.hashCode() : 0);
         result = 31 * result + (checksum != null ? checksum.hashCode() : 0);
         result = 31 * result + type.hashCode();
-        result = 31 * result + (physicalLocation != null ? physicalLocation.hashCode() : 0);
         return result;
     }
 }

commit 9ee4fc60fb74aa7fef841fe79fd05750517ddc74

Fix IBM classloader regression
Migration to Location from String in ClassPathResolver on cbc9fef introduced regression in websphere classloader, location there resolves to "classpath:db/migration/flyway.location" which causes IBM classloader to miss resources, added getPath() like in normal classloader flow, tested on WebSphere 8.5.5.9 jdk8.

flyway-core/src/main/java/org/flywaydb/core/internal/util/scanner/classpath/ClassPathScanner.java
@@ -188,7 +188,7 @@ public class ClassPathScanner implements ResourceAndClassScanner {
 
         if (classLoader.getClass().getName().startsWith("com.ibm")) {
             // WebSphere
-            Enumeration<URL> urls = classLoader.getResources(location + "/flyway.location");
+            Enumeration<URL> urls = classLoader.getResources(location.getPath() + "/flyway.location");
             if (!urls.hasMoreElements()) {
                 LOG.warn("Unable to resolve location " + location + " (ClassLoader: " + classLoader + ")"
                         + " On WebSphere an empty file named flyway.location must be present on the classpath location for WebSphere to find it!");