946c552a5ceb56a7c5e307cd890f3f796fdbf73f
Axel Fontaine <business@axelfontaine.com>
2016-06-08 19:14:38 +0200

    Fixed #1211 PostgreSQL upper case schema names end up double quoted

--- flyway-core/src/test/java/org/flywaydb/core/internal/dbsupport/postgresql/PostgreSQLDbSupportSmallTest.java
+++ flyway-core/src/test/java/org/flywaydb/core/internal/dbsupport/postgresql/PostgreSQLDbSupportSmallTest.java
@@ -20,10 +20,16 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class PostgreSQLDbSupportSmallTest {
+    private final PostgreSQLDbSupport dbSupport = new PostgreSQLDbSupport(null);
+
     @Test
     public void doQuote() {
-        PostgreSQLDbSupport dbSupport = new PostgreSQLDbSupport(null);
         assertEquals("\"abc\"", dbSupport.doQuote("abc"));
         assertEquals("\"a\"\"b\"\"c\"", dbSupport.doQuote("a\"b\"c"));
+    }
+
+    @Test
+    public void getFirstSchemaFromSearchPath() {
+        assertEquals("ABC", dbSupport.getFirstSchemaFromSearchPath("\"ABC\", def"));
     }
 }
