7f9df4909f0b69001fea5332dc112c2995c62957
Pradheep Shanmugam <pradheep.shanmugam@gtnexus.com>
2016-07-19 11:12:24 -0400

    Added MemSQL Specific SQL Support

---
+++ flyway-core/src/main/resources/org/flywaydb/core/internal/dbsupport/MemSQL/MemSQLTable.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2010-2016 Boxfuse GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flywaydb.core.internal.dbsupport.MemSQL;
+
+import org.flywaydb.core.internal.dbsupport.DbSupport;
+import org.flywaydb.core.internal.dbsupport.JdbcTemplate;
+import org.flywaydb.core.internal.dbsupport.Schema;
+import org.flywaydb.core.internal.dbsupport.Table;
+import org.flywaydb.core.internal.util.logging.Log;
+import org.flywaydb.core.internal.util.logging.LogFactory;
+
+import java.sql.SQLException;
+
+/**
+ * MemSQL-specific table.
+ */
+public class MemSQLTable extends Table {
+    private static final Log LOG = LogFactory.getLog(MemSQLTable.class);
+
+    /**
+     * Creates a new MemSQL table.
+     *
+     * @param jdbcTemplate The Jdbc Template for communicating with the DB.
+     * @param dbSupport    The database-specific support.
+     * @param schema       The schema this table lives in.
+     * @param name         The name of the table.
+     */
+    public MemSQLTable(JdbcTemplate jdbcTemplate, DbSupport dbSupport, Schema schema, String name) {
+        super(jdbcTemplate, dbSupport, schema, name);
+    }
+
+    @Override
+    protected void doDrop() throws SQLException {
+        jdbcTemplate.execute("DROP TABLE " + dbSupport.quote(schema.getName(), name));
+    }
+
+    @Override
+    protected boolean doExists() throws SQLException {
+        return exists(schema, null, name);
+    }
+
+    @Override
+    protected void doLock() throws SQLException {
+        LOG.debug("Unable to lock " + this + " as MemSQL does not support SELECT FOR UPDATE. No concurrent migration supported.");
+    }
+}
