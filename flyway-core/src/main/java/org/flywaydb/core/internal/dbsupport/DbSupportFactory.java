91caea77b717e634a787646c4b0f34b83c607407
Axel Fontaine <business@axelfontaine.com>
2017-01-31 12:53:53 +0100

    Fixed #1483 Flyway doesn't recognize Sybase ASE 12.5

--- flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/DbSupportFactory.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/DbSupportFactory.java
@@ -130,8 +130,9 @@ public class DbSupportFactory {
             return new PhoenixDbSupport(connection);
         }
 
-		//Sybase ASE support
-        if (databaseProductName.startsWith("ASE") || databaseProductName.startsWith("Adaptive")) {
+        if (databaseProductName.startsWith("ASE") || databaseProductName.startsWith("Adaptive") //Newer Sybase ASE versions
+                || databaseProductName.startsWith("sql server") // Older Sybase ASE 12.5 installations
+                ) {
         	return new SybaseASEDbSupport(connection);
         }
         if (databaseProductName.startsWith("HDB")) {
