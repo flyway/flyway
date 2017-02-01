e25f7d997e3797fc760a6d23270055e117365bd7
Axel Fontaine <business@axelfontaine.com>
2017-02-01 20:53:46 +0100

    Better diagnostics

--- flyway-core/src/test/java/org/flywaydb/core/migration/ConcurrentMigrationTestCase.java
+++ flyway-core/src/test/java/org/flywaydb/core/migration/ConcurrentMigrationTestCase.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2010-2016 Boxfuse GmbH
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,7 +37,7 @@ import java.util.Map;
 import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 
 /**
  * Test to demonstrate the migration functionality using H2.
@@ -57,9 +57,9 @@ public abstract class ConcurrentMigrationTestCase {
     private String schemaQuoted;
 
     /**
-     * Flag to indicate the concurrent test has failed.
+     * Error message in case the concurrent test has failed.
      */
-    private boolean failed;
+    private String error;
 
     /**
      * The datasource to use for concurrent migration tests.
@@ -123,7 +123,7 @@ public abstract class ConcurrentMigrationTestCase {
                     createFlyway().migrate();
                 } catch (Exception e) {
                     LOG.error("Migrate failed", e);
-                    failed = true;
+                    error = e.getMessage();
                 }
             }
         };
@@ -139,7 +139,7 @@ public abstract class ConcurrentMigrationTestCase {
             threads[i].join();
         }
 
-        assertFalse(failed);
+        assertNull(error, error);
         final MigrationInfo[] applied = flyway.info().applied();
         int expected = 4;
         if (applied[0].getType() == MigrationType.SCHEMA) {
