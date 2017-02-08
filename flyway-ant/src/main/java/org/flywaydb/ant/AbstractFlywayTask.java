2d472b81e1ddb0c227e25d6a18ee1c7baa595618
Axel Fontaine <business@axelfontaine.com>
2017-02-08 02:51:37 +0100

    Improved test stability

--- flyway-ant/src/main/java/org/flywaydb/ant/AbstractFlywayTask.java
+++ flyway-ant/src/main/java/org/flywaydb/ant/AbstractFlywayTask.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2010-2016 Boxfuse GmbH
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -475,7 +475,7 @@ public abstract class AbstractFlywayTask extends Task {
      * a newer version of the application even though it doesn't contain migrations included with an older one anymore.
      *
      * @param ignoreMissingMigrations {@code true} to continue normally and log a warning, {@code false} to fail fast with an exception.
-     * (default: {@code false})
+     *                                (default: {@code false})
      */
     public void setIgnoreMissingMigrations(boolean ignoreMissingMigrations) {
         flyway.setIgnoreMissingMigrations(ignoreMissingMigrations);
