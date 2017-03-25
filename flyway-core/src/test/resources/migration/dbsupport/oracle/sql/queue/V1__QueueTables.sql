947d903da802123822f35b49dd9c9b23ab0b4a19
vosolovskiy <vosolovskiy@users.noreply.github.com>
2017-03-25 22:54:53 +0300

    Support for non-default schemas.
    (cherry picked from commit fe567c6)

--- flyway-core/src/test/resources/migration/dbsupport/oracle/sql/queue/V1__QueueTables.sql
+++ flyway-core/src/test/resources/migration/dbsupport/oracle/sql/queue/V1__QueueTables.sql
@@ -14,12 +14,14 @@
 -- limitations under the License.
 --
 
+declare
+  l_prefix VARCHAR2(131) := '"' || SYS_CONTEXT('USERENV', 'CURRENT_SCHEMA') || '".';
 begin
 --
 ---- Create Queue
-DBMS_AQADM.CREATE_QUEUE_TABLE (queue_table  => 'TEST_QUEUE_MLG_QTAB', queue_payload_type   => 'SYS.AQ$_JMS_TEXT_MESSAGE');
-DBMS_AQADM.CREATE_QUEUE (queue_name  => 'TEST_QUEUE_MLG', queue_table => 'TEST_QUEUE_MLG_QTAB');
-DBMS_AQADM.START_QUEUE ('TEST_QUEUE_MLG');
+DBMS_AQADM.CREATE_QUEUE_TABLE (queue_table  => l_prefix || 'TEST_QUEUE_MLG_QTAB', queue_payload_type   => 'SYS.AQ$_JMS_TEXT_MESSAGE');
+DBMS_AQADM.CREATE_QUEUE (queue_name  => l_prefix || 'TEST_QUEUE_MLG', queue_table => l_prefix || 'TEST_QUEUE_MLG_QTAB');
+DBMS_AQADM.START_QUEUE (l_prefix || 'TEST_QUEUE_MLG');
 --
 
 end;
