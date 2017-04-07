6b2556ba258a054935518bce14811d8075bbed4c
plecaillon <plecaillon@mousquetaires.com>
2017-04-07 19:31:39 +0200

    Add first unit test (for dev only)

---
+++ flyway-core/src/test/java/org/flywaydb/core/internal/dbsupport/teradata/TeradataMigrationTest.java
@@ -0,0 +1,37 @@
+package org.flywaydb.core.internal.dbsupport.teradata;
+
+import org.flywaydb.core.internal.util.jdbc.DriverDataSource;
+import org.flywaydb.core.migration.MigrationTestCase;
+import org.junit.Test;
+import org.springframework.util.Assert;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+public class TeradataMigrationTest extends MigrationTestCase {
+
+  @Override
+  protected DataSource createDataSource(Properties customProperties) throws Exception {
+    String user = customProperties.getProperty("teradata.user", "DBC");
+    String password = customProperties.getProperty("teradata.password", "DBC");
+    String url = customProperties.getProperty("teradata.url", "jdbc:teradata://localhost/DATABASE=DBC,CHARSET=UTF8");
+
+    return new DriverDataSource(Thread.currentThread().getContextClassLoader(), null, url, user, password, null);
+  }
+
+  @Override
+  protected String getQuoteLocation() {
+    return "migration/quote";
+  }
+
+  @Test
+  public void myTest() throws Exception {
+    Assert.notNull(dataSource);
+    Assert.notNull(flyway);
+
+    flyway.setBaselineOnMigrate(true);
+    flyway.setLocations("migration/dbsupport/teradata/");
+    flyway.migrate();
+  }
+}
