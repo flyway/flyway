72e1aac6e9a01db1a0ecd9427d9abfb54e768b6c
plecaillon <plecaillon@mousquetaires.com>
2017-04-07 19:32:14 +0200

    Add Teradata SqlStatement skeleton

---
+++ flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/teradata/TeradataSqlStatementBuilder.java
@@ -0,0 +1,7 @@
+package org.flywaydb.core.internal.dbsupport.teradata;
+
+import org.flywaydb.core.internal.dbsupport.SqlStatementBuilder;
+
+public class TeradataSqlStatementBuilder extends SqlStatementBuilder {
+
+}
