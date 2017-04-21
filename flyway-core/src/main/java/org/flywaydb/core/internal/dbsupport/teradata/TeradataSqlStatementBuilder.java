3efa98c46ff13b2ac7d9f10ecdf994d8f9fccc64
plecaillon <plecaillon@mousquetaires.com>
2017-04-21 15:57:40 +0200

    Add DDL detection to prevent migration execution in tx

--- flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/teradata/TeradataSqlStatementBuilder.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/teradata/TeradataSqlStatementBuilder.java
@@ -1,7 +1,32 @@
 package org.flywaydb.core.internal.dbsupport.teradata;
 
 import org.flywaydb.core.internal.dbsupport.SqlStatementBuilder;
+import org.flywaydb.core.internal.util.StringUtils;
 
 public class TeradataSqlStatementBuilder extends SqlStatementBuilder {
 
+  /**
+   * Holds the beginning of the statement.
+   */
+  private String statementStart = "";
+
+  @Override
+  protected void applyStateChanges(String line) {
+    super.applyStateChanges(line);
+
+    if (!executeInTransaction) {
+      return;
+    }
+
+    if (StringUtils.countOccurrencesOf(statementStart, " ") < 8) {
+      statementStart += line;
+      statementStart += " ";
+      statementStart = statementStart.replaceAll("\\s+", " ");
+    }
+
+    // If a DDL transaction is detected we do not execute the migration in a transaction.
+    if (statementStart.matches("(?i)(ALTER|BEGIN|CREATE|DROP|GRANT|RENAME|REPLACE|REVOKE|RESTART) .*")) {
+      executeInTransaction = false;
+    }
+  }
 }
