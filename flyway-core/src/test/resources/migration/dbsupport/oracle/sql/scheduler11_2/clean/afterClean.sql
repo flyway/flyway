e4a330c62697e915a2f61694a3afdd1119be52bc
Victor Osolovskiy <vosolovskiy@users.noreply.github.com>
2017-06-04 16:05:55 +0300

    Fixed broken license headers

--- flyway-core/src/test/resources/migration/dbsupport/oracle/sql/scheduler11_2/clean/afterClean.sql
+++ flyway-core/src/test/resources/migration/dbsupport/oracle/sql/scheduler11_2/clean/afterClean.sql
@@ -1,4 +1,10 @@
 --
+-- Copyright 2010-2017 Boxfuse GmbH
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
 --         http://www.apache.org/licenses/LICENSE-2.0
 --
 -- Unless required by applicable law or agreed to in writing, software
