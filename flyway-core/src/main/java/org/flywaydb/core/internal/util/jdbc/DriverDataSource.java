3c913631b5b75a105f1381ea8d178f5cef934e72
Igor Nikolaev <igor.nikolaev@zalando.fi>
2017-06-07 23:00:27 +0300

    Support TestContainers JDBC connection string

--- flyway-core/src/main/java/org/flywaydb/core/internal/util/jdbc/DriverDataSource.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/util/jdbc/DriverDataSource.java
@@ -247,6 +247,10 @@ public class DriverDataSource implements DataSource {
      * @return The Jdbc driver.
      */
     private String detectDriverForUrl(String url) {
+        if (url.startsWith("jdbc:tc:")) {
+            return "org.testcontainers.jdbc.ContainerDatabaseDriver";
+        }
+
         if (url.startsWith("jdbc:db2:")) {
             return "com.ibm.db2.jcc.DB2Driver";
         }
