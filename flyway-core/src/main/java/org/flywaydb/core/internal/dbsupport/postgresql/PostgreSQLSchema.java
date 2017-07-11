0175d3e477b11f0432f05e094f73b996f285c3b3
RobertRad <RobertRad@users.noreply.github.com>
2017-07-11 14:07:27 +0200

    PostgreSQL: repair Schema.doEmpty()

--- flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/postgresql/PostgreSQLSchema.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/dbsupport/postgresql/PostgreSQLSchema.java
@@ -48,7 +48,7 @@ public class PostgreSQLSchema extends Schema<PostgreSQLDbSupport> {
 
     @Override
     protected boolean doEmpty() throws SQLException {
-        return jdbcTemplate.queryForBoolean("SELECT EXISTS (   SELECT 1\n" +
+        return !jdbcTemplate.queryForBoolean("SELECT EXISTS (   SELECT 1\n" +
                 "   FROM   pg_catalog.pg_class c\n" +
                 "   JOIN   pg_catalog.pg_namespace n ON n.oid = c.relnamespace\n" +
                 "   WHERE  n.nspname = ?)", name);
