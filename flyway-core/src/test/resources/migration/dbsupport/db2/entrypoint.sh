2d74c7dfd2dfd8b4e3c1d3f10a2c07db1d5866b9
Axel Fontaine <business@axelfontaine.com>
2017-09-07 12:26:15 +0200

    Updated repositories

--- flyway-core/src/test/resources/migration/dbsupport/db2/entrypoint.sh
+++ flyway-core/src/test/resources/migration/dbsupport/db2/entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 echo -e "$DB2INST1_PASSWORD\n$DB2INST1_PASSWORD" | passwd db2inst1
 su - db2inst1 -c "db2start"
 su - db2inst1 -c "db2 -tvf /createDatabase.sql"
-nohup /usr/sbin/sshd -D 2>&1 > /dev/null &
+
 while true; do sleep 1000; done
