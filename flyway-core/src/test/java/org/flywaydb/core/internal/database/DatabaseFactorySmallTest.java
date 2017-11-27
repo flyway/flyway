6ca3474630201ecc58fb549db4ed4a31fd8967d0
Axel Fontaine <business@axelfontaine.com>
2017-11-27 21:24:49 +0100

    Fixed URL filtering

--- flyway-core/src/test/java/org/flywaydb/core/internal/database/DatabaseFactorySmallTest.java
+++ flyway-core/src/test/java/org/flywaydb/core/internal/database/DatabaseFactorySmallTest.java
@@ -11,5 +11,6 @@ public class DatabaseFactorySmallTest {
         assertEquals("jdbc:postgresql://host/db", DatabaseFactory.filterUrl("jdbc:postgresql://admin:<password>@host/db"));
         assertEquals("jdbc:postgresql://host/db", DatabaseFactory.filterUrl("jdbc:postgresql://host/db?user=admin&password=<password>"));
         assertEquals("jdbc:sqlserver:////host:1234;databaseName=db", DatabaseFactory.filterUrl("jdbc:sqlserver:////host:1234;databaseName=db"));
+        assertEquals("jdbc:sap://localhost:62060/?databaseName=HXE", DatabaseFactory.filterUrl("jdbc:sap://localhost:62060/?databaseName=HXE"));
     }
 }
