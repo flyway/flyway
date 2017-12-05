0ffe1541625c32060871229f1f4f47d01b49e33b
Axel Fontaine <business@axelfontaine.com>
2017-12-05 14:09:18 +0100

    Moved to internal release license

--- flyway-core/src/main/java/org/flywaydb/core/internal/util/AsciiTable.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/util/AsciiTable.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2010-2017 Boxfuse GmbH
+ *
+ * INTERNAL RELEASE. ALL RIGHTS RESERVED.
+ */
 package org.flywaydb.core.internal.util;
 
 import java.util.ArrayList;
---
+++ ossify.cmd
@@ -0,0 +1,40 @@
+@REM
+@REM Copyright 2010-2017 Boxfuse GmbH
+@REM
+@REM INTERNAL RELEASE. ALL RIGHTS RESERVED.
+@REM
+
+@echo off
+
+echo ============== BUILDING OSSIFIER
+cd c:\Workspaces\flyway-ossifier
+call mvn clean package || goto :error
+echo ============== RUNNING OSSIFIER
+java -jar target\flyway-ossifier-1.0-SNAPSHOT.jar
+
+echo ============== BUILDING COMMUNITY
+cd c:\Workspaces\flyway
+call mvn clean install -T3 || goto :error
+
+echo ============== BUILDING PRO
+cd c:\Workspaces\flyway-pro
+call mvn clean install -T3 || goto :error
+
+echo ============== BUILDING ENTERPRISE
+cd c:\Workspaces\flyway-enterprise
+call mvn clean install -T3 || goto :error
+
+echo ============== BUILDING TRIAL
+cd c:\Workspaces\flyway-trial
+call mvn clean install -T3 || goto :error
+
+echo ============== SUCCESS
+cd c:\Workspaces\flyway-master
+pause
+goto :EOF
+
+:error
+echo ============== FAILED WITH ERROR %errorlevel%
+cd c:\Workspaces\flyway-master
+pause
+exit /b %errorlevel%
