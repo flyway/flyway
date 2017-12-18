b245e5e8f66f70944c00564ab1f0bae095921814
Axel Fontaine <business@axelfontaine.com>
2017-12-18 23:48:21 +0100

    Validate should compare abbreviated descriptions

---
+++ flyway-core/src/main/java/org/flywaydb/core/internal/util/AbbreviationUtils.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2010-2017 Boxfuse GmbH
+ *
+ * INTERNAL RELEASE. ALL RIGHTS RESERVED.
+ *
+ * Must
+ * be
+ * exactly
+ * 13 lines
+ * to match
+ * community
+ * edition
+ * license
+ * length.
+ */
+package org.flywaydb.core.internal.util;
+
+/**
+ * Various abbreviation-related utilities.
+ */
+public class AbbreviationUtils {
+    /**
+     * Prevents instantiation.
+     */
+    private AbbreviationUtils() {
+        // Do nothing.
+    }
+
+    /**
+     * Abbreviates this description to a length that will fit in the database.
+     *
+     * @param description The description to process.
+     * @return The abbreviated version.
+     */
+    public static String abbreviateDescription(String description) {
+        if (description == null) {
+            return null;
+        }
+
+        if (description.length() <= 200) {
+            return description;
+        }
+
+        return description.substring(0, 197) + "...";
+    }
+
+    /**
+     * Abbreviates this script to a length that will fit in the database.
+     *
+     * @param script The script to process.
+     * @return The abbreviated version.
+     */
+    public static String abbreviateScript(String script) {
+        if (script == null) {
+            return null;
+        }
+
+        if (script.length() <= 1000) {
+            return script;
+        }
+
+        return "..." + script.substring(3, 1000);
+    }
+}
