fcde6d620f0d985d41d23b5b3a0a16c1b7834f0e
Axel Fontaine <business@axelfontaine.com>
2018-01-02 15:34:17 +0100

    Updated license to 2018

--- flyway-core/src/test/java/org/flywaydb/core/FlywayMediumTest.java
+++ flyway-core/src/test/java/org/flywaydb/core/FlywayMediumTest.java
@@ -121,7 +121,7 @@ public class FlywayMediumTest {
         assertEquals(MigrationState.ABOVE_TARGET, flyway.info().all()[3].getState());
 
         flyway.migrate();
-        assertEquals(64671501, flyway.info().current().getChecksum().intValue());
+        assertEquals(-1617192283, flyway.info().current().getChecksum().intValue());
         assertEquals("1.1", flyway.info().current().getVersion().toString());
         assertEquals(MigrationState.SUCCESS, flyway.info().current().getState());
         assertEquals(4, flyway.info().all().length);
