ac791ee331dafbbaa6236861ff5918b19a977a11
Yannik Hampe <yzeugs@cipher-code.de>
2018-02-17 19:53:50 +0100

    Do not delete types that extensions depend on when cleaning PostgreSql

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/postgresql/PostgreSQLSchema.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/postgresql/PostgreSQLSchema.java
@@ -146,9 +146,11 @@ public class PostgreSQLSchema extends Schema<PostgreSQLDatabase> {
         List<Map<String, String>> rows =
                 jdbcTemplate.queryForList(
                         "select typname, typcategory from pg_catalog.pg_type t "
+                                + "left join pg_depend dep on dep.objid = t.oid and dep.deptype = 'e' "
                                 + "where (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid)) and "
                                 + "NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid) and "
-                                + "t.typnamespace in (select oid from pg_catalog.pg_namespace where nspname = ?)",
+                                + "t.typnamespace in (select oid from pg_catalog.pg_namespace where nspname = ?) and "
+                                + "dep.objid is null",
                         name);
 
         List<String> statements = new ArrayList<>();
