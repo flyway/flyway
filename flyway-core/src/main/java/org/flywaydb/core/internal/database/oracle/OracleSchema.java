e9269626f2e27e12dd0b6694385be23e0d5b9cf4
Axel Fontaine <business@axelfontaine.com>
2018-03-06 10:37:29 +0100

    Fixed #1943: Flyway schema creation issue on Oracle 12.2 DB with password policy enabled

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/oracle/OracleSchema.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/oracle/OracleSchema.java
@@ -79,7 +79,8 @@ public class OracleSchema extends Schema<OracleDatabase> {
 
     @Override
     protected void doCreate() throws SQLException {
-        jdbcTemplate.execute("CREATE USER " + database.quote(name) + " IDENTIFIED BY flyway");
+        jdbcTemplate.execute("CREATE USER " + database.quote(name) + " IDENTIFIED BY "
+                + database.quote("FFllyywwaayy00!!"));
         jdbcTemplate.execute("GRANT RESOURCE TO " + database.quote(name));
         jdbcTemplate.execute("GRANT UNLIMITED TABLESPACE TO " + database.quote(name));
     }
