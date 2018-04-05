570d80c353378fa185da2158b3675543980674b8
cristhiank <calovi86@gmail.com>
2018-04-04 22:51:49 -0500

    proposed fix for #1970 CockroachDB 2.0 support

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/cockroachdb/CockroachDBTable.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/cockroachdb/CockroachDBTable.java
@@ -15,50 +15,51 @@
  */
 package org.flywaydb.core.internal.database.cockroachdb;
 
+import java.sql.SQLException;
+
 import org.flywaydb.core.api.logging.Log;
 import org.flywaydb.core.api.logging.LogFactory;
 import org.flywaydb.core.internal.database.Database;
-import org.flywaydb.core.internal.util.jdbc.JdbcTemplate;
 import org.flywaydb.core.internal.database.Schema;
 import org.flywaydb.core.internal.database.Table;
-
-import java.sql.SQLException;
+import org.flywaydb.core.internal.util.jdbc.JdbcTemplate;
 
 /**
  * CockroachDB-specific table.
  */
 public class CockroachDBTable extends Table {
-    private static final Log LOG = LogFactory.getLog(CockroachDBTable.class);
+	private static final Log LOG = LogFactory.getLog( CockroachDBTable.class );
 
-    /**
-     * Creates a new CockroachDB table.
-     *
-     * @param jdbcTemplate The Jdbc Template for communicating with the DB.
-     * @param database    The database-specific support.
-     * @param schema       The schema this table lives in.
-     * @param name         The name of the table.
-     */
-    CockroachDBTable(JdbcTemplate jdbcTemplate, Database database, Schema schema, String name) {
-        super(jdbcTemplate, database, schema, name);
-    }
+	/**
+	 * Creates a new CockroachDB table.
+	 *
+	 * @param jdbcTemplate The Jdbc Template for communicating with the DB.
+	 * @param database The database-specific support.
+	 * @param schema The schema this table lives in.
+	 * @param name The name of the table.
+	 */
+	CockroachDBTable(JdbcTemplate jdbcTemplate, Database database, Schema schema, String name) {
+		super( jdbcTemplate, database, schema, name );
+	}
 
-    @Override
-    protected void doDrop() throws SQLException {
-        jdbcTemplate.execute("DROP TABLE " + database.quote(schema.getName(), name) + " CASCADE");
-    }
+	@Override
+	protected void doDrop() throws SQLException {
+		jdbcTemplate.execute( "DROP TABLE " + database.quote( schema.getName(), name ) + " CASCADE" );
+	}
 
-    @Override
-    protected boolean doExists() throws SQLException {
-        return jdbcTemplate.queryForBoolean("SELECT EXISTS (\n" +
-                "   SELECT 1\n" +
-                "   FROM   information_schema.tables \n" +
-                "   WHERE  table_schema = ?\n" +
-                "   AND    table_name = ?\n" +
-                ")", schema.getName(), name);
-    }
+	@Override
+	protected boolean doExists() throws SQLException {
+		return jdbcTemplate.queryForBoolean( "SELECT EXISTS (\n" +
+													 "   SELECT 1\n" +
+													 "   FROM   information_schema.tables \n" +
+													 "   WHERE  table_catalog = ?\n" +
+													 "   AND    table_schema = 'public'\n" +
+													 "   AND    table_name = ?\n" +
+													 ")", schema.getName(), name );
+	}
 
-    @Override
-    protected void doLock() {
-        LOG.debug("Unable to lock " + this + " as CockroachDB does not support locking. No concurrent migration supported.");
-    }
+	@Override
+	protected void doLock() {
+		LOG.debug( "Unable to lock " + this + " as CockroachDB does not support locking. No concurrent migration supported." );
+	}
 }
