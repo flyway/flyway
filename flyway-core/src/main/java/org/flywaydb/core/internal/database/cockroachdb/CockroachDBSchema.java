98c4a2bdb955265e570f6e0f62ae234ae1e51dbb
Axel Fontaine <business@axelfontaine.com>
2018-04-09 08:58:46 +0200

    Fixed #1970: CockroachDB 2.0 support

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/cockroachdb/CockroachDBSchema.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/cockroachdb/CockroachDBSchema.java
@@ -15,11 +15,9 @@
  */
 package org.flywaydb.core.internal.database.cockroachdb;
 
-import org.flywaydb.core.api.FlywayException;
-import org.flywaydb.core.internal.util.StringUtils;
-import org.flywaydb.core.internal.util.jdbc.JdbcTemplate;
 import org.flywaydb.core.internal.database.Schema;
 import org.flywaydb.core.internal.database.Table;
+import org.flywaydb.core.internal.util.jdbc.JdbcTemplate;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -30,10 +28,10 @@ import java.util.List;
  */
 public class CockroachDBSchema extends Schema<CockroachDBDatabase> {
     /**
-     * Creates a new PostgreSQL schema.
+     * Creates a new CockroachDB schema.
      *
      * @param jdbcTemplate The Jdbc Template for communicating with the DB.
-     * @param database    The database-specific support.
+     * @param database     The database-specific support.
      * @param name         The name of the schema.
      */
     CockroachDBSchema(JdbcTemplate jdbcTemplate, CockroachDBDatabase database, String name) {
@@ -42,15 +40,26 @@ public class CockroachDBSchema extends Schema<CockroachDBDatabase> {
 
     @Override
     protected boolean doExists() throws SQLException {
-        return jdbcTemplate.queryForInt("SELECT COUNT(*) FROM pg_namespace WHERE nspname=?", name) > 0;
+        return jdbcTemplate.queryForBoolean("SELECT EXISTS ( SELECT 1 FROM pg_database WHERE datname=? )", name);
     }
 
     @Override
     protected boolean doEmpty() throws SQLException {
-        int objectCount = jdbcTemplate.queryForInt(
-                "SELECT count(*) FROM information_schema.tables WHERE table_schema=? AND table_type='BASE TABLE'",
-                name);
-        return objectCount == 0;
+        if (database.getMajorVersion() == 1) {
+            return !jdbcTemplate.queryForBoolean("SELECT EXISTS (" +
+                    "  SELECT 1" +
+                    "  FROM information_schema.tables" +
+                    "  WHERE table_schema=?" +
+                    "  AND table_type='BASE TABLE'" +
+                    ")", name);
+        }
+        return !jdbcTemplate.queryForBoolean("SELECT EXISTS (" +
+                "  SELECT 1" +
+                "  FROM information_schema.tables " +
+                "  WHERE table_catalog=?" +
+                "  AND table_schema='public'" +
+                "  AND table_type='BASE TABLE'" +
+                ")", name);
     }
 
     @Override
@@ -87,8 +96,7 @@ public class CockroachDBSchema extends Schema<CockroachDBDatabase> {
                         "SELECT relname FROM pg_catalog.pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace" +
                                 // that don't depend on an extension
                                 " LEFT JOIN pg_depend dep ON dep.objid = c.oid AND dep.deptype = 'e'" +
-                                " WHERE c.relkind = 'v' AND  n.nspname = ? AND dep.objid IS NULL",
-                        name);
+                                " WHERE c.relkind = 'v' AND n.nspname = 'public' AND dep.objid IS NULL");
         List<String> statements = new ArrayList<>();
         for (String domainName : viewNames) {
             statements.add("DROP VIEW IF EXISTS " + database.quote(name, domainName) + " CASCADE");
@@ -99,16 +107,27 @@ public class CockroachDBSchema extends Schema<CockroachDBDatabase> {
 
     @Override
     protected Table[] doAllTables() throws SQLException {
-        List<String> tableNames =
-                jdbcTemplate.queryForStringList(
-                        //Search for all the table names
-                        "SELECT table_name FROM information_schema.tables" +
-                                //in this schema
-                                " WHERE table_schema=?" +
-                                //that are real tables (as opposed to views)
-                                " AND table_type='BASE TABLE'",
-                        name
-                );
+        String query;
+        if (database.getMajorVersion() == 1) {
+            query =
+                    //Search for all the table names
+                    "SELECT table_name FROM information_schema.tables" +
+                            //in this schema
+                            " WHERE table_schema=?" +
+                            //that are real tables (as opposed to views)
+                            " AND table_type='BASE TABLE'";
+        } else {
+            query =
+                    //Search for all the table names
+                    "SELECT table_name FROM information_schema.tables" +
+                            //in this database
+                            " WHERE table_catalog=?" +
+                            " AND table_schema='public'" +
+                            //that are real tables (as opposed to views)
+                            " AND table_type='BASE TABLE'";
+        }
+
+        List<String> tableNames = jdbcTemplate.queryForStringList(query, name);
         //Views and child tables are excluded as they are dropped with the parent table when using cascade.
 
         Table[] tables = new Table[tableNames.size()];
