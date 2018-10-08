659b002f5cf39e7240e514c478e2462a28927eb3
Thomas Golden <tgolden@andplus.com>
2018-10-08 10:01:50 -0400

    Updates changes to multi-dex scanning for 5.x codebase

--- flyway-core/src/main/java/org/flywaydb/core/internal/scanner/android/AndroidScanner.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/scanner/android/AndroidScanner.java
@@ -16,6 +16,8 @@
 package org.flywaydb.core.internal.scanner.android;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import dalvik.system.DexFile;
 import org.flywaydb.core.api.FlywayException;
 import org.flywaydb.core.api.Location;
@@ -27,6 +29,7 @@ import org.flywaydb.core.internal.resource.android.AndroidResource;
 import org.flywaydb.core.internal.scanner.classpath.ResourceAndClassScanner;
 import org.flywaydb.core.internal.util.ClassUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -79,30 +82,93 @@ public class AndroidScanner implements ResourceAndClassScanner {
 
         List<Class<?>> classes = new ArrayList<Class<?>>();
         String sourceDir = context.getApplicationInfo().sourceDir;
-        DexFile dex = null;
+
+
+
+        /*
+         * Android applications can consist of multiple dex files (c.f. https://developer.android.com/studio/build/multidex)
+         * Because of this, we can't only scan the primary dex file at sourceDir.
+         * If we did, we might miss Java-based migrations that are located in the secondary dex files.
+         * Apps which target API 21+ _should_ be fine as their dex files are precompiled into a single .oat file, but
+         * apps which target 20 or lower (e.g. KitKat) will potentially have this issue.
+         */
+        List<DexFile> dexFiles = new ArrayList<>();
+        List<File> searchPaths = new ArrayList<>();
+
         try {
-            dex = new DexFile(sourceDir);
+            //There's always at least one dex file, located at sourceDir
+            dexFiles.add(new DexFile(sourceDir));
+        } catch (IOException e) {
+            LOG.error("Unable to load dex file " + sourceDir, e);
+        }
+
+        String dexPrefix = new File(sourceDir).getName() + ".classes";
+
+        //The location where the secondary dexes are stored has changed over time and across phone models
+        searchPaths.add(new File(context.getFilesDir(), "secondary-dexes"));
+        searchPaths.add(new File(new File(context.getApplicationInfo().dataDir, "code_cache"), "secondary-dexes"));
+
+        //How many secondary dex files do we expect?
+        SharedPreferences prefs = context.getSharedPreferences("multidex.version", Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
+            ? Context.MODE_PRIVATE
+            : Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS);
+        //If this preference doesn't exist, assume that means there's only the primary dex file
+        int totalDexes = prefs.getInt("dex.number", 1);
+
+        //We need a writeable directory to load dex files into - use the cache directory to avoid stomping user files
+        File scratchDir = context.getCacheDir();
+
+        //If there really is only a single dex file, this entire loop is skipped
+        tryDexes: for(int secondaryIndex = 2; secondaryIndex <= totalDexes; secondaryIndex++) {
+            String expectedZip = dexPrefix + secondaryIndex + ".zip";
+            File temp = new File(scratchDir, dexPrefix + secondaryIndex + ".tmp");
+
+            try {
+                tryPossibilities: for(File possibility : searchPaths) {
+                    File zip = new File(possibility, expectedZip);
+                    if(!zip.exists()) {
+                        // Keep looking in our search paths
+                        continue tryPossibilities;
+                    }
+                    dexFiles.add(DexFile.loadDex(zip.getAbsolutePath(), temp.getAbsolutePath(), 0));
+                    // Keep looking for more dexes
+                    continue tryDexes;
+                }
+                //The inner loop concluded without loading any dex files - this is unexpected
+                LOG.warn("Unable to find a dex file named " + expectedZip);
+            } catch (IOException e) {
+                LOG.error("Unable to load dex file " + expectedZip, e);
+            }
+        }
+
+        //Now that we've added all of the dex files we can find, scan them all for classes
+        for(DexFile dex : dexFiles) {
             Enumeration<String> entries = dex.entries();
-            while (entries.hasMoreElements()) {
+            while(entries.hasMoreElements()) {
                 String className = entries.nextElement();
-                if (className.startsWith(pkg)) {
+                if(className.startsWith(pkg)) {
                     Class<?> clazz = ClassUtils.loadClass(className, classLoader);
-                    if (clazz != null) {
+                    if(clazz != null) {
                         classes.add(clazz);
                     }
                 }
             }
-        } catch (IOException e) {
-            LOG.warn("Unable to scan DEX file (" + sourceDir + "): " + e.getMessage());
-        } finally {
-            if (dex != null) {
-                try {
-                    dex.close();
-                } catch (IOException e) {
-                    LOG.debug("Unable to close DEX file (" + sourceDir + "): " + e.getMessage());
-                }
+            try {
+                dex.close();
+            } catch (IOException e) {
+                LOG.error("Unable to close dex file " + dex.getName(), e);
             }
         }
+
+        //Clean up the temp files we created to extract dexes to
+        for(int secondaryIndex = 2; secondaryIndex <= totalDexes; secondaryIndex++) {
+            File temp = new File(scratchDir, dexPrefix + secondaryIndex + ".tmp");
+            boolean success = temp.delete();
+            if(!success) {
+                LOG.warn("Unable to manually delete temporary file " + temp.getName() + " in cache directory");
+            }
+        }
+
         return classes;
     }
 }
