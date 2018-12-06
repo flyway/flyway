de844a08e9dbc13c9ba2bbc2b459fdb0952ee12f
Axel Fontaine <business@axelfontaine.com>
2018-12-06 16:10:19 +0100

    Additional fixes for #2232

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/postgresql/PostgreSQLSchema.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/postgresql/PostgreSQLSchema.java
@@ -239,7 +239,14 @@ public class PostgreSQLSchema extends Schema<PostgreSQLDatabase> {
     private List<String> generateDropStatementsForDomains() throws SQLException {
         List<String> domainNames =
                 jdbcTemplate.queryForStringList(
-                        "SELECT domain_name FROM information_schema.domains WHERE domain_schema=?", name);
+                        "SELECT t.typname as domain_name\n" +
+                                "FROM pg_catalog.pg_type t\n" +
+                                "       LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace\n" +
+                                "       LEFT JOIN pg_depend dep ON dep.objid = t.oid AND dep.deptype = 'e'\n" +
+                                "WHERE t.typtype = 'd'\n" +
+                                "  AND n.nspname = ?\n" +
+                                "  AND dep.objid IS NULL"
+                        , name);
 
         List<String> statements = new ArrayList<>();
         for (String domainName : domainNames) {
