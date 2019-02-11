6cecb73555f149e9eb6d864ee762fa39945df5d0
Axel Fontaine <axel@boxfuse.com>
2019-02-11 11:40:48 +0100

    Fixed #2266: Flyway does not clean sequences in MariaDB

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/mysql/MySQLSchema.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/mysql/MySQLSchema.java
@@ -87,6 +87,11 @@ public class MySQLSchema extends Schema<MySQLDatabase> {
             table.drop();
         }
         jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1");
+
+        // MariaDB 10.3 and newer only
+        for (String statement : cleanSequences()) {
+            jdbcTemplate.execute(statement);
+        }
     }
 
     /**
@@ -147,10 +152,30 @@ public class MySQLSchema extends Schema<MySQLDatabase> {
         return statements;
     }
 
+    /**
+     * Generate the statements to clean the sequences in this schema.
+     *
+     * @return The list of statements.
+     * @throws SQLException when the clean statements could not be generated.
+     */
+    private List<String> cleanSequences() throws SQLException {
+        List<String> names =
+                jdbcTemplate.queryForStringList(
+                        "SELECT table_name FROM information_schema.tables WHERE table_schema=?" +
+                                " AND table_type='SEQUENCE'", name);
+
+        List<String> statements = new ArrayList<>();
+        for (String name : names) {
+            statements.add("DROP SEQUENCE " + database.quote(this.name, name));
+        }
+        return statements;
+    }
+
     @Override
     protected Table[] doAllTables() throws SQLException {
         List<String> tableNames = jdbcTemplate.queryForStringList(
-                "SELECT table_name FROM information_schema.tables WHERE table_schema=? AND table_type='BASE TABLE'", name);
+                "SELECT table_name FROM information_schema.tables WHERE table_schema=?" +
+                        " AND table_type='BASE TABLE'", name);
 
         Table[] tables = new Table[tableNames.size()];
         for (int i = 0; i < tableNames.size(); i++) {
