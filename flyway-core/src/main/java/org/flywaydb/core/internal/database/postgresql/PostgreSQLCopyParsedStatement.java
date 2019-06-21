ec2b2c1b44af0e82719bdfb0bc71db62a7ee61d0
Axel Fontaine <axel@boxfuse.com>
2019-06-21 15:38:10 +0200

    Fixed #2355: gradle flywayMigrate failing on missing postgresql CopyManager

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/postgresql/PostgreSQLCopyParsedStatement.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/postgresql/PostgreSQLCopyParsedStatement.java
@@ -15,17 +15,20 @@
  */
 package org.flywaydb.core.internal.database.postgresql;
 
+import org.flywaydb.core.api.FlywayException;
 import org.flywaydb.core.internal.jdbc.JdbcTemplate;
 import org.flywaydb.core.internal.jdbc.Result;
 import org.flywaydb.core.internal.jdbc.Results;
 import org.flywaydb.core.internal.sqlscript.Delimiter;
 import org.flywaydb.core.internal.sqlscript.ParsedSqlStatement;
 import org.flywaydb.core.internal.sqlscript.SqlScriptExecutor;
-import org.postgresql.copy.CopyManager;
-import org.postgresql.core.BaseConnection;
 
-import java.io.IOException;
+import java.io.Reader;
 import java.io.StringReader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
@@ -61,17 +64,37 @@ public class PostgreSQLCopyParsedStatement extends ParsedSqlStatement {
 
 
     ) {
+        // #2355: Use reflection to ensure this works in cases where the PostgreSQL driver classes were loaded in a
+        //        child URLClassLoader instead of the system classloader.
+        Object baseConnection;
+        Object copyManager;
+        Method copyManagerCopyInMethod;
+        try {
+            Connection connection = jdbcTemplate.getConnection();
+            ClassLoader classLoader = connection.getClass().getClassLoader();
+
+            Class<?> baseConnectionClass = classLoader.loadClass("org.postgresql.core.BaseConnection");
+            baseConnection = connection.unwrap(baseConnectionClass);
+
+            Class<?> copyManagerClass = classLoader.loadClass("org.postgresql.copy.CopyManager");
+            Constructor<?> copyManagerConstructor = copyManagerClass.getConstructor(baseConnectionClass);
+            copyManagerCopyInMethod = copyManagerClass.getMethod("copyIn", String.class, Reader.class);
+
+            copyManager = copyManagerConstructor.newInstance(baseConnection);
+        } catch (Exception e) {
+            throw new FlywayException("Unable to find PostgreSQL CopyManager class", e);
+        }
+
         Results results = new Results();
         try {
-            CopyManager copyManager = new CopyManager(jdbcTemplate.getConnection().unwrap(BaseConnection.class));
             try {
-                long updateCount = copyManager.copyIn(getSql(), new StringReader(copyData));
+                Long updateCount = (Long) copyManagerCopyInMethod.invoke(copyManager, getSql(), new StringReader(copyData));
                 results.addResult(new Result(updateCount
 
 
 
                 ));
-            } catch (IOException e) {
+            } catch (IllegalAccessException | InvocationTargetException e) {
                 throw new SQLException("Unable to execute COPY operation", e);
             }
         } catch (SQLException e) {
