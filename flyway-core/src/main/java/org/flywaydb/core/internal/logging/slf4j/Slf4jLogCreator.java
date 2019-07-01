3c3dd97bbfdfb54635d2f6f5cbfbb5e56b9722e2
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-07-01 19:27:26 +0800

    Disable SLF4j's loggerNameMismatch warning
    See http://www.slf4j.org/codes.html#loggerNameMismatch

--- flyway-core/src/main/java/org/flywaydb/core/internal/logging/slf4j/Slf4jLogCreator.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/logging/slf4j/Slf4jLogCreator.java
@@ -24,6 +24,6 @@ import org.slf4j.LoggerFactory;
  */
 public class Slf4jLogCreator implements LogCreator {
     public Log createLogger(Class<?> clazz) {
-        return new Slf4jLog(LoggerFactory.getLogger(clazz));
+        return new Slf4jLog(LoggerFactory.getLogger(clazz.getName()));
     }
 }
