84be840109b003f98d1bea50c636a6c98ee9ba38
Julia Hayward <Julia.Hayward@red-gate.com>
2019-07-25 14:15:15 +0100

    Fixed #2435: DB2 11.5 support

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/db2/DB2Database.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/db2/DB2Database.java
@@ -58,13 +58,14 @@ public class DB2Database extends Database<DB2Connection> {
 
 
 
+
     @Override
     public final void ensureSupported() {
         ensureDatabaseIsRecentEnough("9.7");
 
         ensureDatabaseNotOlderThanOtherwiseRecommendUpgradeToFlywayEdition("11.1", org.flywaydb.core.internal.license.Edition.ENTERPRISE);
 
-        recommendFlywayUpgradeIfNecessary("11.1");
+        recommendFlywayUpgradeIfNecessary("11.5");
     }
 
     @Override
