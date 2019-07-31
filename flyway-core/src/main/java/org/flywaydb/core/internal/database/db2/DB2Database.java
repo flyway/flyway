e89c3729799c91f923a3607c94438bdbbfc55d09
Julia Hayward <julia.hayward@red-gate.com>
2019-07-31 16:52:40 +0100

    Minor cleanup

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/db2/DB2Database.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/db2/DB2Database.java
@@ -95,9 +95,9 @@ public class DB2Database extends Database<DB2Connection> {
 
 
                 + tablespace + ";\n" +
-                (baseline ? getBaselineStatement(table) + ";\n" : "") +
                 "ALTER TABLE " + table + " ADD CONSTRAINT \"" + table.getName() + "_pk\" PRIMARY KEY (\"installed_rank\");\n" +
-                "CREATE INDEX \"" + table.getSchema().getName() + "\".\"" + table.getName() + "_s_idx\" ON " + table + " (\"success\");";
+                "CREATE INDEX \"" + table.getSchema().getName() + "\".\"" + table.getName() + "_s_idx\" ON " + table + " (\"success\");" +
+                (baseline ? getBaselineStatement(table) + ";\n" : "");
     }
 
     @Override
