21120109e888ce7be075879c8eee495efc64c39e
Артем Гордеев <gordeevart@gmail.com>
2020-09-17 15:27:07 +0300

    fix: Add exasol external script parsing

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/exasol/ExasolParser.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/exasol/ExasolParser.java
@@ -15,9 +15,15 @@
  */
 package org.flywaydb.core.internal.database.exasol;
 
+import java.io.IOException;
 import org.flywaydb.core.api.configuration.Configuration;
 import org.flywaydb.core.internal.parser.Parser;
+import org.flywaydb.core.internal.parser.ParserContext;
 import org.flywaydb.core.internal.parser.ParsingContext;
+import org.flywaydb.core.internal.parser.PeekingReader;
+import org.flywaydb.core.internal.parser.StatementType;
+import org.flywaydb.core.internal.parser.Token;
+import org.flywaydb.core.internal.parser.TokenType;
 import org.flywaydb.core.internal.sqlscript.Delimiter;
 
 /**
@@ -27,6 +33,11 @@ import org.flywaydb.core.internal.sqlscript.Delimiter;
  */
 public class ExasolParser extends Parser {
 
+    public static final StatementType SCRIPT = new StatementType();
+    public static final String CREATE_SCRIPT_REGEXP
+      = "create\\s+(or\\s+replace\\s+)*(python|python3|lua|java|r|script).*";
+    public static final String SCRIPT_DELIMITER = "/";
+
     public ExasolParser(final Configuration configuration, final ParsingContext parsingContext) {
         super(configuration, parsingContext, 3);
     }
@@ -34,5 +45,38 @@ public class ExasolParser extends Parser {
     @Override
     protected Delimiter getDefaultDelimiter() {
         return new Delimiter(";", true);
+    }
+
+    @Override
+    protected boolean isDelimiter(final String peek, final ParserContext context, final int col) {
+
+        if (context.getStatementType().equals(SCRIPT)) {
+            return peek.startsWith(SCRIPT_DELIMITER);
+        }
+
+        return super.isDelimiter(peek, context, col);
+    }
+
+    @Override
+    protected StatementType detectStatementType(final String simplifiedStatement) {
+
+        if (simplifiedStatement.toLowerCase().matches(CREATE_SCRIPT_REGEXP)) {
+            return SCRIPT;
+        }
+
+        return super.detectStatementType(simplifiedStatement);
+    }
+
+    @Override
+    protected Token handleDelimiter(final PeekingReader reader, final ParserContext context, final int pos,
+                                    final int line, final int col)
+      throws IOException {
+
+        final String text
+          = context.getStatementType().equals(SCRIPT) ? SCRIPT_DELIMITER : context.getDelimiter().getDelimiter();
+
+        reader.swallow(text.length());
+
+        return new Token(TokenType.DELIMITER, pos, line, col, text, text, context.getParensDepth());
     }
 }
