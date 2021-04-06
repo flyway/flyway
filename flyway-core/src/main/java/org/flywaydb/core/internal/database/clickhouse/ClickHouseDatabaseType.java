ebe3770aa1266be67bc8e9779cff4b05a1ebe63d
k.struzhkin <koztyastruga@gmail.com>
2021-04-06 11:37:54 +0300

    Add support clickhouse

---
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/clickhouse/ClickHouseDatabaseType.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright © Red Gate Software Ltd 2010-2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.flywaydb.core.internal.database.clickhouse;
+
+import org.flywaydb.core.api.ResourceProvider;
+import org.flywaydb.core.api.configuration.Configuration;
+import org.flywaydb.core.internal.database.base.Database;
+import org.flywaydb.core.internal.database.base.DatabaseType;
+import org.flywaydb.core.internal.jdbc.JdbcConnectionFactory;
+import org.flywaydb.core.internal.jdbc.StatementInterceptor;
+import org.flywaydb.core.internal.parser.Parser;
+import org.flywaydb.core.internal.parser.ParsingContext;
+
+import java.sql.Connection;
+import java.sql.Types;
+
+public class ClickHouseDatabaseType extends DatabaseType {
+
+    @Override
+    public String getName() {
+        return "ClickHouse";
+    }
+
+    @Override
+    public int getNullType() {
+        return Types.NULL;
+    }
+
+    @Override
+    public boolean handlesJDBCUrl(String url) {
+        return url.startsWith("jdbc:clickhouse:");
+    }
+
+    @Override
+    public String getDriverClass(String url, ClassLoader classLoader) {
+        return "ru.yandex.clickhouse.ClickHouseDriver";
+    }
+
+    @Override
+    public boolean handlesDatabaseProductNameAndVersion(String databaseProductName, String databaseProductVersion,
+                                                        Connection connection) {
+        return databaseProductName.startsWith("ClickHouse");
+    }
+
+    @Override
+    public Database<ClickHouseConnection> createDatabase(Configuration configuration,
+                                                         JdbcConnectionFactory jdbcConnectionFactory,
+                                                         StatementInterceptor statementInterceptor) {
+        return new ClickHouseDatabase(configuration, jdbcConnectionFactory, statementInterceptor);
+    }
+
+    @Override
+    public Parser createParser(Configuration configuration, ResourceProvider resourceProvider,
+                               ParsingContext parsingContext) {
+        return new ClickHouseParser(configuration, parsingContext);
+    }
+
+    @Override
+    public boolean detectUserRequiredByUrl(String url) {
+        return !url.contains("user=");
+    }
+
+    @Override
+    public boolean detectPasswordRequiredByUrl(String url) {
+        // ClickHouse supports password in URL
+        return !url.contains("password=");
+    }
+
+}
