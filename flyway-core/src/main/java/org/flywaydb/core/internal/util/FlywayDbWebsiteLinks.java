5aa1a82c792a25cf3ba846e5b567cc09b9fd8b79
rg-buildmonkey <github-buildmonkey@red-gate.com>
2022-08-18 11:26:47 +0100

    Bump version to flyway-9.1.6
    Please see the GH release for the release notes
    
    Fixes #3499 connectRetriesInterval now defaults to 120 instead of 0 on the Maven plugin.

--- flyway-core/src/main/java/org/flywaydb/core/internal/util/FlywayDbWebsiteLinks.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/util/FlywayDbWebsiteLinks.java
@@ -48,4 +48,5 @@ public class FlywayDbWebsiteLinks {
     public static final String TEAMS_FEATURES_FOR_CLOUD_SPANNER = "https://rd.gt/2ZvELhV";
     public static final String FILE_ENCODING_HELP = "https://rd.gt/3BzSFhr";
     public static final String TEAMS_ENTERPRISE_DOWNLOAD = "https://rd.gt/3aqhTXb";
+    public static final String SQLFLUFF_CONFIGURATION = "https://rd.gt/3dDFeFN";
 }
