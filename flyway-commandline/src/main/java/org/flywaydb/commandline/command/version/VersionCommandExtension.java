ffe7df64dbb6f532380b989327b97b11cf354508
rg-buildmonkey <github-buildmonkey@red-gate.com>
2022-10-19 17:00:18 +0100

    Bump version to flyway-9.5.0
    Please see the GH release for the release notes
    
    fix #3539: Add support for PostgreSQL 15
    
    fix #3519: SQLPlus - double &s for placeholders
    
    Update commons-text to fix vulnerability CVE-2022-42889
    
    Add edition to the version model
    
    When urls are unsupported by check, ascertain which ones are unsupported, and include them as part of the exception.

--- flyway-commandline/src/main/java/org/flywaydb/commandline/command/version/VersionCommandExtension.java
+++ flyway-commandline/src/main/java/org/flywaydb/commandline/command/version/VersionCommandExtension.java
@@ -57,7 +57,7 @@ public class VersionCommandExtension implements CommandExtension {
         LOG.debug("Java " + System.getProperty("java.version") + " (" + System.getProperty("java.vendor") + ")");
         LOG.debug(System.getProperty("os.name") + " " + System.getProperty("os.version") + " " + System.getProperty("os.arch") + "\n");
 
-        return new VersionResult(VersionPrinter.getVersion(), command);
+        return new VersionResult(VersionPrinter.getVersion(), command, VersionPrinter.EDITION);
     }
 
     @Override
