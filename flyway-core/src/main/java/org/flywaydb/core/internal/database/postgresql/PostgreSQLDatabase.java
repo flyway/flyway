e361d8e8efc67fd3c2f12300b175795549282b78
rg-buildmonkey <github-buildmonkey@red-gate.com>
2022-10-26 14:16:05 +0100

    Bump version to flyway-9.6.0
    Please see the GH release for the release notes
    
    Move SQLFluff specific components into own module
    
    Add error override behavior to exclude full message
    
    Bump hsqldb version to resolve CVE-2022-41853
    
    fix #3489: PostgreSQL history table indexes and constraints now use the configured tablespace
    
    Fix #3516: Drop PostgreSQL domains with CASCADE
    
    Add support for building a darwin-arm64 flyway-commandline assembly

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/postgresql/PostgreSQLDatabase.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/postgresql/PostgreSQLDatabase.java
@@ -83,8 +83,8 @@ public class PostgreSQLDatabase extends Database<PostgreSQLConnection> {
                 "    \"success\" BOOLEAN NOT NULL\n" +
                 ")" + tablespace + ";\n" +
                 (baseline ? getBaselineStatement(table) + ";\n" : "") +
-                "ALTER TABLE " + table + " ADD CONSTRAINT \"" + table.getName() + "_pk\" PRIMARY KEY (\"installed_rank\");\n" +
-                "CREATE INDEX \"" + table.getName() + "_s_idx\" ON " + table + " (\"success\");";
+                "ALTER TABLE " + table + " ADD CONSTRAINT \"" + table.getName() + "_pk\" PRIMARY KEY (\"installed_rank\")" + (configuration.getTablespace() != null ? " USING INDEX" + tablespace : "" ) + ";\n" +
+                "CREATE INDEX \"" + table.getName() + "_s_idx\" ON " + table + " (\"success\")" + tablespace + ";";
     }
 
     @Override
