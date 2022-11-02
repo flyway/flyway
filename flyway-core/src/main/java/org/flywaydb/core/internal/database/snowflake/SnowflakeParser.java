ead6a7a0b12b5aa3bcd63b2f9d4f34854357e5df
rg-buildmonkey <github-buildmonkey@red-gate.com>
2022-11-02 13:47:34 +0000

    Bump version to flyway-9.7.0
    Please see the GH release for the release notes
    
    Add rule code to HTML report for code analysis
    
    Update MSAL4J to fix CVE-2022-42004
    
    fix #3552: Fix Snowflake parser to handle nested 'BEGIN...END' statements
    
    update aws-secretsmanager to fix CVE-2022-42004

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/snowflake/SnowflakeParser.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/snowflake/SnowflakeParser.java
@@ -41,14 +41,17 @@ public class SnowflakeParser extends Parser {
         String alternativeQuoteOpen = ALTERNATIVE_QUOTE;
         String alternativeQuoteEnd = ALTERNATIVE_QUOTE;
 
+        String text;
         if (reader.peek(ALTERNATIVE_QUOTE_SCRIPT)) {
-            alternativeQuoteOpen = ALTERNATIVE_QUOTE_SCRIPT;
+            alternativeQuoteOpen = "BEGIN";
             alternativeQuoteEnd = "END";
+            reader.swallowUntilExcluding(alternativeQuoteOpen);
+            text = readBetweenRecursive(reader, alternativeQuoteOpen, alternativeQuoteEnd);
+        } else {
+            reader.swallow(alternativeQuoteOpen.length());
+            text = reader.readUntilExcluding(alternativeQuoteOpen, alternativeQuoteEnd);
+            reader.swallow(alternativeQuoteEnd.length());
         }
-
-        reader.swallow(alternativeQuoteOpen.length());
-        String text = reader.readUntilExcluding(alternativeQuoteEnd);
-        reader.swallow(alternativeQuoteEnd.length());
 
         return new Token(TokenType.STRING, pos, line, col, text, text, context.getParensDepth());
     }
@@ -56,5 +59,18 @@ public class SnowflakeParser extends Parser {
     @Override
     protected boolean isSingleLineComment(String peek, ParserContext context, int col) {
         return peek.startsWith("--") || peek.startsWith("//");
+    }
+
+    private String readBetweenRecursive(PeekingReader reader, String prefix, String suffix) throws IOException {
+        StringBuilder result = new StringBuilder();
+        reader.swallow(prefix.length());
+        while (!reader.peek(suffix)) {
+            result.append(reader.readUntilExcluding(prefix, suffix));
+            if (reader.peek(prefix)) {
+                result.append(prefix).append(readBetweenRecursive(reader, prefix, suffix)).append(suffix);
+            }
+        }
+        reader.swallow(suffix.length());
+        return result.toString();
     }
 }
